24363625ece7e1e61a93055497cdb643808ebe53
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2013-09-17 14:30:09 +0200

    set test to ignore

--- atm-corr-processor/src/test/java/org/esa/beam/dataio/atmcorr/AtmCorrProcessBuilderTest.java
+++ atm-corr-processor/src/test/java/org/esa/beam/dataio/atmcorr/AtmCorrProcessBuilderTest.java
@@ -1,5 +1,6 @@
 package org.esa.beam.dataio.atmcorr;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -9,7 +10,7 @@ public class AtmCorrProcessBuilderTest {
 
     //this test will only work when the atmospheric correction processor is installed
     @Test
-//    @Ignore
+    @Ignore
     public void testAtmCorrProcessBuilder() throws Exception {
         AtmCorrProcessBuilder builder = new AtmCorrProcessBuilder();
         String l1cProductPath = "/home/tonio/S2L2APP/testdata/Level-1C_User_Product";
