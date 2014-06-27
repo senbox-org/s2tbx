196406768a08b43a5f3e7738daba728737708025
Julien Malik <julien.malik@gmail.com>
2014-06-27 12:03:46 +0200

    remove nest-op-sentinel1 nest-io-sentinel1 beam-sentinel3-reader

--- beam-sentinel3-reader/TODO.md
+++
@@ -1,4 +0,0 @@
-BEAM problems
-=============
-* timeCoordinates.nc can not be read. The Variable has a rank of 1.
-  Such variables are ignored by NetCDF CF reader.
--- beam-sentinel3-reader/src/main/java/org/esa/beam/dataio/s3/synergy/TileRectangleCalculator.java
+++
@@ -1,8 +0,0 @@
-package org.esa.beam.dataio.s3.synergy;
-
-import java.awt.Rectangle;
-
-interface TileRectangleCalculator {
-
-    Rectangle[] calculateTileRectangles(int columnCount, int rowCount);
-}
