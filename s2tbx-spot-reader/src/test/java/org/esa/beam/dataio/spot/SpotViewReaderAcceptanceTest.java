955822c427f6641de6f0a64dedf2679bd5eaacb1
Ramona Manda <ramona.manda@c-s.ro>
2014-09-12 11:46:01 +0300

    Added json test files for SpotDimap, SpotView, RapidEyeL1 and RapidEyeL3
    Added json test files for SpotDimap, SpotView, RapidEyeL1 and RapidEyeL3

---
+++ s2tbx-spot-reader/src/test/java/org/esa/beam/dataio/spot/SpotViewReaderAcceptanceTest.java
@@ -0,0 +1,9 @@
+package org.esa.beam.dataio.spot;
+
+import org.esa.beam.dataio.ProductReaderAcceptanceTest;
+
+/**
+ * Created by kraftek on 9/11/2014.
+ */
+public class SpotViewReaderAcceptanceTest extends ProductReaderAcceptanceTest {
+}
