785abd49c9b1dc03bcf4986989bc062c4decbf07
oscarpicas <oscar.picas@gmail.com>
2014-09-23 14:57:21 +0200

    Fix for long filepaths in Windows systems

--- s2tbx-reader/src/test/java/org/esa/beam/dataio/s2/ShortenTest.java
+++ s2tbx-reader/src/test/java/org/esa/beam/dataio/s2/ShortenTest.java
@@ -25,4 +25,28 @@ public class ShortenTest {
             }
         }
     }
+
+    @Test
+    public void testVeryLongFileName() throws Exception
+    {
+        String visualStudioPath = "D:\\is tmp\\TheDataIsVeryVeryLongHere\\S2A_OPER_PRD_MSIL1C_PDMC_20130621T120000_R065_V20091211T165928_20091211T170025.SAFE\\GRANULE\\S2A_OPER_MSI_L1C_TL_CGS1_20130621T120000_A000065_T14SLH_N01.01\\IMG_DATA\\S2A_OPER_MSI_L1C_TL_CGS1_20130621T120000_A000065_T14SLH_B04.jp2";
+
+        if(SystemUtils.IS_OS_WINDOWS)
+        {
+            String shortPath = Utils.GetIterativeShortPathName(visualStudioPath);
+            Assert.assertEquals("D:\\ISTMP~1\\THEDAT~1\\S2A_OP~1.SAF\\GRANULE\\S270DA~1.01\\IMG_DATA\\S2A_OP~4.JP2", shortPath);
+        }
+    }
+
+    @Test
+    public void testVeryLongFileName2() throws Exception
+    {
+        String visualStudioPath = "D:\\is tmp\\TheDataIsVeryVeryLongHere\\S2A_OPER_PRD_MSIL1C_PDMC_20130621T120000_R065_V20091211T165928_20091211T170025.SAFE\\GRANULE\\S2A_OPER_MSI_L1C_TL_CGS1_20130621T120000_A000065_T14SLH_N01.01\\IMG_DATA\\S2A_OPER_MSI_L1C_TL_CGS1_20130621T120000_A000065_T14SLH_B04.jpx";
+
+        if(SystemUtils.IS_OS_WINDOWS)
+        {
+            String shortPath = Utils.GetIterativeShortPathName(visualStudioPath);
+            Assert.assertEquals("", shortPath);
+        }
+    }
 }
