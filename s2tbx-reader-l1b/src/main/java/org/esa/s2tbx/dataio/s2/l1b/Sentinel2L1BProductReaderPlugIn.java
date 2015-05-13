32ebaf822861f4a5e34c96b8e74bf5feaea0451c
marpet <marco.peters@brockmann-consult.de>
2015-05-13 16:32:05 +0200

    update imports
    somehow the renaming of BeamFileChooser was incomplete

--- s2tbx-reader-l1b/src/main/java/org/esa/s2tbx/dataio/s2/l1b/Sentinel2L1BProductReaderPlugIn.java
+++ s2tbx-reader-l1b/src/main/java/org/esa/s2tbx/dataio/s2/l1b/Sentinel2L1BProductReaderPlugIn.java
@@ -22,6 +22,7 @@ import org.esa.s2tbx.dataio.s2.l1b.filepatterns.S2L1bProductFilename;
 import org.esa.snap.framework.dataio.DecodeQualification;
 import org.esa.snap.framework.dataio.ProductReader;
 import org.esa.snap.framework.dataio.ProductReaderPlugIn;
+import org.esa.snap.util.io.SnapFileFilter;
 import org.esa.snap.util.logging.BeamLogManager;
 
 import java.io.File;
