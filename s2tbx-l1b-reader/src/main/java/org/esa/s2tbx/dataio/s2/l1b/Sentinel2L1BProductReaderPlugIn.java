b40b230dabdacdcaf79154fc63d2ca7c8d125eed
Oscar Picas <oscar.picas@gmail.com>
2015-05-14 18:57:17 +0200

    Temporary commit

--- s2tbx-l1b-reader/src/main/java/org/esa/s2tbx/dataio/s2/l1b/Sentinel2L1BProductReaderPlugIn.java
+++ s2tbx-l1b-reader/src/main/java/org/esa/s2tbx/dataio/s2/l1b/Sentinel2L1BProductReaderPlugIn.java
@@ -58,7 +58,7 @@ public class Sentinel2L1BProductReaderPlugIn implements ProductReaderPlugIn {
     @Override
     public ProductReader createReaderInstance() {
         BeamLogManager.getSystemLogger().info("Building product reader...");
-        return new Sentinel2L1BProductReader(this, false);
+        return new Sentinel2L1BProductReader(this, true);
     }
 
     @Override
