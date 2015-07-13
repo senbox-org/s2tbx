fc4d0ec7b4facfcd77ae377e75cd85d0632ec785
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-07-13 23:35:09 +0200

    Fixing SNAP-122: Missing about box SPI

---
+++ s2tbx-kit/src/main/java/org/esa/s2tbx/about/S2tbxAboutBox.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.esa.s3tbx.about;
+
+import javax.swing.JLabel;
+import org.esa.snap.rcp.about.AboutBox;
+
+/**
+ * @author Norman
+ */
+@AboutBox(displayName = "S2TBX", position = 20)
+public class S2tbxAboutBox extends JLabel {
+    
+    public S2tbxAboutBox() {
+        super("<html>This is the wonderful <b>Sentinel-2 Toolbox</b>");
+    }
+}
