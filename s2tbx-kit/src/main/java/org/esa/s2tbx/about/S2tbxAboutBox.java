351925306847a06c503b30d3a5330b0050500c10
Norman Fomferra <norman.fomferra@brockmann-consult.de>
2015-07-14 15:09:29 +0200

    Fixed SNAP-121, SNAP-122

--- s2tbx-kit/src/main/java/org/esa/s2tbx/about/S2tbxAboutBox.java
+++ s2tbx-kit/src/main/java/org/esa/s2tbx/about/S2tbxAboutBox.java
@@ -5,16 +5,30 @@
  */
 package org.esa.s2tbx.about;
 
-import javax.swing.JLabel;
 import org.esa.snap.rcp.about.AboutBox;
+import org.openide.modules.ModuleInfo;
+import org.openide.modules.Modules;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
 
 /**
  * @author Norman
  */
 @AboutBox(displayName = "S2TBX", position = 20)
-public class S2tbxAboutBox extends JLabel {
-    
+public class S2tbxAboutBox extends JPanel {
+
     public S2tbxAboutBox() {
-        super("<html>This is the wonderful <b>Sentinel-2 Toolbox</b>");
+        super(new BorderLayout(4, 4));
+        setBorder(new EmptyBorder(4, 4, 4, 4));
+        ModuleInfo moduleInfo = Modules.getDefault().ownerOf(S2tbxAboutBox.class);
+        ImageIcon aboutImage = new ImageIcon(S2tbxAboutBox.class.getResource("about_s2tbx.jpg"));
+        JLabel iconLabel = new JLabel(aboutImage);
+        add(iconLabel, BorderLayout.CENTER);
+        add(new JLabel("<html><b>Sentinel-2 Toolbox (S2TBX) version " + moduleInfo.getImplementationVersion() + "</b>", SwingConstants.RIGHT), BorderLayout.SOUTH);
     }
 }
