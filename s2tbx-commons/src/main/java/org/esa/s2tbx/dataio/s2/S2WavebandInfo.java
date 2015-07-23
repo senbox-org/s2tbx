b66f023e921eb3c9f7dbf7e26d02bb793252b922
Julien Malik <julien.malik@paraiso.me>
2015-07-23 19:33:41 +0200

    Order initializer

--- s2tbx-commons/src/main/java/org/esa/s2tbx/dataio/s2/S2WavebandInfo.java
+++ s2tbx-commons/src/main/java/org/esa/s2tbx/dataio/s2/S2WavebandInfo.java
@@ -33,8 +33,8 @@ public class S2WavebandInfo {
                    double bandwidth) {
         this.bandId = bandId;
         this.bandName = bandName;
+        this.resolution = resolution;
         this.wavelength = wavelength;
         this.bandwidth = bandwidth;
-        this.resolution = resolution;
     }
 }
