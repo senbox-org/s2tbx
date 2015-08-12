9ac95cd02bdecbc0562f7d96f86ff4f7d6338b76
Cosmin Cara <kraftek@c-s.ro>
2015-08-12 16:12:58 +0300

    Small convenience change for J2K Reader in BandMatrix. Added parralel loops support.

--- s2tbx-commons/src/main/java/org/esa/s2tbx/dataio/Parallel.java
+++ s2tbx-commons/src/main/java/org/esa/s2tbx/dataio/Parallel.java
@@ -8,43 +8,55 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 /**
- * Created by kraftek on 7/16/2015.
+ * Helper class to execute loops in parallel.
  */
 public class Parallel {
-    static final int iCPU = Runtime.getRuntime().availableProcessors();
-
-    public interface LoopBody <T> {
+    static final int PARALLELISM = Runtime.getRuntime().availableProcessors();
+    /**
+     * The loop code wrapper
+     */
+    public interface Runnable<T> {
         void run(T i);
     }
 
-    public static <T> void ForEach(Iterable <T> parameters, final LoopBody<T> loopBody) {
-        ExecutorService executor = Executors.newFixedThreadPool(iCPU);
-        List<Future<?>> futures  = new LinkedList<>();
-        for (final T param : parameters) {
-            Future<?> future = executor.submit(() -> loopBody.run(param));
-            futures.add(future);
+    /**
+     * Iterates an Iterable collection in parallel.
+     *
+     * @param params    The items to iterate
+     * @param code      The loop code
+     * @param <T>       The type of items
+     */
+    public static <T> void ForEach(Iterable <T> params, final Runnable<T> code) {
+        ExecutorService executor = Executors.newFixedThreadPool(PARALLELISM);
+        List<Future<?>> tasks  = new LinkedList<>();
+        for (final T param : params) {
+            tasks.add(executor.submit(() -> code.run(param)));
         }
-        for (Future<?> f : futures) {
-            try   { f.get(); }
+        tasks.stream().forEach(task -> {
+            try   { task.get(); }
             catch (InterruptedException | ExecutionException ignored) { }
-        }
+        });
         executor.shutdown();
     }
 
-    public static void For(int start, int stop, final LoopBody<Integer> loopBody) {
-        ExecutorService executor = Executors.newFixedThreadPool(iCPU);
-        List<Future<?>> futures  = new LinkedList<>();
-
-        for (int i=start; i<stop; i++) {
-            final Integer k = i;
-            Future<?> future = executor.submit(() -> loopBody.run(k));
-            futures.add(future);
+    /**
+     * Runs a for-loop in parallel.
+     *
+     * @param start     The start index
+     * @param stop      The end index
+     * @param code      The loop code
+     */
+    public static void For(int start, int stop, final Runnable<Integer> code) {
+        ExecutorService executor = Executors.newFixedThreadPool(PARALLELISM);
+        List<Future<?>> tasks  = new LinkedList<>();
+        for (int i = start; i < stop; i++) {
+            final Integer index = i;
+            tasks.add(executor.submit(() -> code.run(index)));
         }
-        for (Future<?> f : futures) {
-            try   { f.get(); }
+        tasks.stream().forEach(task -> {
+            try   { task.get(); }
             catch (InterruptedException | ExecutionException ignored) { }
-        }
-
+        });
         executor.shutdown();
     }
 }
