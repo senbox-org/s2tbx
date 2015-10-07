9c7c21aa459df21a6db0b36832f85eebf48da15f
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2015-10-07 13:37:26 +0200

    replaced Os by 0s

--- s2tbx-s2msi-reader/src/main/java/org/esa/s2tbx/dataio/s2/ortho/S2OrthoProduct10MReaderPlugIn.java
+++ s2tbx-s2msi-reader/src/main/java/org/esa/s2tbx/dataio/s2/ortho/S2OrthoProduct10MReaderPlugIn.java
@@ -51,6 +51,6 @@ public abstract class S2OrthoProduct10MReaderPlugIn extends S2OrthoProductReader
 
     @Override
     public String getDescription(Locale locale) {
-        return String.format("Sentinel-2 MSI %s - 1Om bands - %s", getLevel(), epsgToDisplayName(getEPSG()));
+        return String.format("Sentinel-2 MSI %s - 10m bands - %s", getLevel(), epsgToDisplayName(getEPSG()));
     }
 }
