f681745178f46faadaa1436db6179ebb0df2afe0
Dragos Mihailescu <dmihailescu@c-s.ro>
2016-01-25 19:13:39 +0200

    Add PSSRa index

---
+++ s2tbx-radiometric-indices/src/main/java/org/esa/s2tbx/radiometry/PssraOp.java
@@ -0,0 +1,117 @@
+package org.esa.s2tbx.radiometry;
+
+import com.bc.ceres.core.ProgressMonitor;
+import org.esa.snap.core.datamodel.Band;
+import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.gpf.OperatorException;
+import org.esa.snap.core.gpf.OperatorSpi;
+import org.esa.snap.core.gpf.Tile;
+import org.esa.snap.core.gpf.annotations.OperatorMetadata;
+import org.esa.snap.core.gpf.annotations.Parameter;
+
+import java.awt.*;
+import java.util.Map;
+
+@OperatorMetadata(
+        alias = "PssraOp",
+        category = "Optical/Thematic Land Processing",
+        description = "Pigment Specific Simple Ratio, chlorophyll index",
+        authors = "Dragos Mihailescu",
+        copyright = "Copyright (C) 2016 by CS ROMANIA")
+public class PssraOp extends BaseIndexOp{
+
+    // constants
+    public static final String BAND_NAME = "pssra";
+
+    @Parameter(label = "Red factor", defaultValue = "1.0F", description = "The value of the red source band is multiplied by this value.")
+    private float redFactor;
+
+    @Parameter(label = "NIR factor", defaultValue = "1.0F", description = "The value of the NIR source band is multiplied by this value.")
+    private float nirFactor;
+
+    @Parameter(label = "Red source band",
+            description = "The red band for the PSSRa computation. If not provided, the " +
+                    "operator will try to find the best fitting band.",
+            rasterDataNodeType = Band.class)
+    private String redSourceBand;
+
+    @Parameter(label = "NIR source band",
+            description = "The near-infrared band for the PSSRa computation. If not provided," +
+                    " the operator will try to find the best fitting band.",
+            rasterDataNodeType = Band.class)
+    private String nirSourceBand;
+
+    @Override
+    public String getBandName() {
+        return BAND_NAME;
+    }
+
+    @Override
+    public void computeTileStack(Map<Band, Tile> targetTiles, Rectangle rectangle, ProgressMonitor pm) throws OperatorException {
+        pm.beginTask("Computing PSSRa", rectangle.height);
+        try {
+            Tile redTile = getSourceTile(getSourceProduct().getBand(redSourceBand), rectangle);
+            Tile nirTile = getSourceTile(getSourceProduct().getBand(nirSourceBand), rectangle);
+
+            Tile pssra = targetTiles.get(targetProduct.getBand(BAND_NAME));
+            Tile pssraFlags = targetTiles.get(targetProduct.getBand(FLAGS_BAND_NAME));
+
+            float pssraValue;
+            int pssraFlagsValue;
+
+            for (int y = rectangle.y; y < rectangle.y + rectangle.height; y++) {
+                for (int x = rectangle.x; x < rectangle.x + rectangle.width; x++) {
+                    final float nir = nirFactor * nirTile.getSampleFloat(x, y);
+                    final float red = redFactor * redTile.getSampleFloat(x, y);
+
+                    pssraValue = nir / red; /* verify if red = 0 */
+
+                    pssraFlagsValue = 0;
+                    if (Float.isNaN(pssraValue) || Float.isInfinite(pssraValue)) {
+                        pssraFlagsValue |= ARITHMETIC_FLAG_VALUE;
+                        pssraValue = 0.0f;
+                    }
+                    if (pssraValue < 0.0f) {
+                        pssraFlagsValue |= LOW_FLAG_VALUE;
+                    }
+                    if (pssraValue > 1.0f) {
+                        pssraFlagsValue |= HIGH_FLAG_VALUE;
+                    }
+                    pssra.setSample(x, y, pssraValue);
+                    pssraFlags.setSample(x, y, pssraFlagsValue);
+                }
+                checkForCancellation();
+                pm.worked(1);
+            }
+        } finally {
+            pm.done();
+        }
+    }
+
+    @Override
+    protected void loadSourceBands(Product product) throws OperatorException {
+        if (redSourceBand == null) {
+            redSourceBand = findBand(650, 680, product); /* (600, 650) */
+            getLogger().info("Using band '" + redSourceBand + "' as red input band.");
+        }
+        if (nirSourceBand == null) {
+            nirSourceBand = findBand(773, 793, product); /* (800, 900) */
+            getLogger().info("Using band '" + nirSourceBand + "' as NIR input band.");
+        }
+        if (redSourceBand == null) {
+            throw new OperatorException("Unable to find band that could be used as red input band. Please specify band.");
+        }
+        if (nirSourceBand == null) {
+            throw new OperatorException("Unable to find band that could be used as nir input band. Please specify band.");
+        }
+    }
+
+    public static class Spi extends OperatorSpi {
+
+        public Spi() {
+            super(PssraOp.class);
+        }
+
+    }
+
+}
