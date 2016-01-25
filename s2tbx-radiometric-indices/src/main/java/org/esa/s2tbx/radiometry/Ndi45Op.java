0a3c05b68954e70cc574ca7ab73ef9808cc7ed72
Dragos Mihailescu <dmihailescu@c-s.ro>
2016-01-25 19:39:39 +0200

    Add NDI45 index

---
+++ s2tbx-radiometric-indices/src/main/java/org/esa/s2tbx/radiometry/Ndi45Op.java
@@ -0,0 +1,117 @@
+package org.esa.s2tbx.radiometry;
+
+import com.bc.ceres.core.ProgressMonitor;
+import org.esa.snap.core.datamodel.Band;
+import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.gpf.OperatorException;
+import org.esa.snap.core.gpf.OperatorSpi;
+import org.esa.snap.core.gpf.Tile;
+import org.esa.snap.core.gpf.annotations.OperatorMetadata;
+import org.esa.snap.core.gpf.annotations.Parameter;
+
+import java.awt.*;
+import java.util.Map;
+
+@OperatorMetadata(
+        alias = "Ndi45Op",
+        category = "Optical/Thematic Land Processing",
+        description = "Normalized Difference Index using bands 4 and 5",
+        authors = "Dragos Mihailescu",
+        copyright = "Copyright (C) 2016 by CS ROMANIA")
+public class Ndi45Op extends BaseIndexOp{
+
+    // constants
+    public static final String BAND_NAME = "ndi45";
+
+    @Parameter(label = "Red factor", defaultValue = "1.0F", description = "The value of the red source band is multiplied by this value.")
+    private float redFactor;
+
+    @Parameter(label = "NIR factor", defaultValue = "1.0F", description = "The value of the NIR source band is multiplied by this value.")
+    private float nirFactor;
+
+    @Parameter(label = "Red source band",
+            description = "The red band for the NDI45 computation. If not provided, the " +
+                    "operator will try to find the best fitting band.",
+            rasterDataNodeType = Band.class)
+    private String redSourceBand;
+
+    @Parameter(label = "NIR source band",
+            description = "The near-infrared band for the NDI45 computation. If not provided," +
+                    " the operator will try to find the best fitting band.",
+            rasterDataNodeType = Band.class)
+    private String nirSourceBand;
+
+    @Override
+    public String getBandName() {
+        return BAND_NAME;
+    }
+
+    @Override
+    public void computeTileStack(Map<Band, Tile> targetTiles, Rectangle rectangle, ProgressMonitor pm) throws OperatorException {
+        pm.beginTask("Computing NDI45", rectangle.height);
+        try {
+            Tile redTile = getSourceTile(getSourceProduct().getBand(redSourceBand), rectangle);
+            Tile nirTile = getSourceTile(getSourceProduct().getBand(nirSourceBand), rectangle);
+
+            Tile ndi45 = targetTiles.get(targetProduct.getBand(BAND_NAME));
+            Tile ndi45Flags = targetTiles.get(targetProduct.getBand(FLAGS_BAND_NAME));
+
+            float ndi45Value;
+            int ndi45FlagsValue;
+
+            for (int y = rectangle.y; y < rectangle.y + rectangle.height; y++) {
+                for (int x = rectangle.x; x < rectangle.x + rectangle.width; x++) {
+                    final float nir = nirFactor * nirTile.getSampleFloat(x, y);
+                    final float red = redFactor * redTile.getSampleFloat(x, y);
+
+                    ndi45Value = (nir - red) / (nir + red);
+
+                    ndi45FlagsValue = 0;
+                    if (Float.isNaN(ndi45Value) || Float.isInfinite(ndi45Value)) {
+                        ndi45FlagsValue |= ARITHMETIC_FLAG_VALUE;
+                        ndi45Value = 0.0f;
+                    }
+                    if (ndi45Value < 0.0f) {
+                        ndi45FlagsValue |= LOW_FLAG_VALUE;
+                    }
+                    if (ndi45Value > 1.0f) {
+                        ndi45FlagsValue |= HIGH_FLAG_VALUE;
+                    }
+                    ndi45.setSample(x, y, ndi45Value);
+                    ndi45Flags.setSample(x, y, ndi45FlagsValue);
+                }
+                checkForCancellation();
+                pm.worked(1);
+            }
+        } finally {
+            pm.done();
+        }
+    }
+
+    @Override
+    protected void loadSourceBands(Product product) throws OperatorException {
+        if (redSourceBand == null) {
+            redSourceBand = findBand(650, 680, product); /* (600, 650) */
+            getLogger().info("Using band '" + redSourceBand + "' as red input band.");
+        }
+        if (nirSourceBand == null) {
+            nirSourceBand = findBand(698, 713, product); /* (800, 900) */
+            getLogger().info("Using band '" + nirSourceBand + "' as NIR input band.");
+        }
+        if (redSourceBand == null) {
+            throw new OperatorException("Unable to find band that could be used as red input band. Please specify band.");
+        }
+        if (nirSourceBand == null) {
+            throw new OperatorException("Unable to find band that could be used as nir input band. Please specify band.");
+        }
+    }
+
+    public static class Spi extends OperatorSpi {
+
+        public Spi() {
+            super(Ndi45Op.class);
+        }
+
+    }
+
+}
