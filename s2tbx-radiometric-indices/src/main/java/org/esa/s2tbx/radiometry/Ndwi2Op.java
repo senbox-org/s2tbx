aac51fc5afc461257102525604e132b390904bde
Dragos Mihailescu <dmihailescu@c-s.ro>
2016-01-28 13:12:56 +0200

    Add NDWI2 index and update the existing NDWI one

--- s2tbx-radiometric-indices/src/main/java/org/esa/s2tbx/radiometry/Ndwi2Op.java
+++ s2tbx-radiometric-indices/src/main/java/org/esa/s2tbx/radiometry/Ndwi2Op.java
@@ -13,30 +13,30 @@ import java.awt.*;
 import java.util.Map;
 
 @OperatorMetadata(
-        alias = "NdwiOp",
+        alias = "Ndwi2Op",
         category = "Optical/Thematic Land Processing",
-        description = "The Normalized Difference Water Index was developed for the extraction of water features",
+        description = "The Normalized Difference Water Index, allowing for the measurement of surface water extent",
         authors = "Dragos Mihailescu",
         copyright = "Copyright (C) 2016 by CS ROMANIA")
-public class NdwiOp extends BaseIndexOp{
+public class Ndwi2Op extends BaseIndexOp{
 
     // constants
-    public static final String BAND_NAME = "ndwi";
+    public static final String BAND_NAME = "ndwi2";
 
-    @Parameter(label = "MIR factor", defaultValue = "1.0F", description = "The value of the MIR source band is multiplied by this value.")
-    private float mirFactor;
+    @Parameter(label = "Green factor", defaultValue = "1.0F", description = "The value of the green source band is multiplied by this value.")
+    private float greenFactor;
 
     @Parameter(label = "NIR factor", defaultValue = "1.0F", description = "The value of the NIR source band is multiplied by this value.")
     private float nirFactor;
 
-    @Parameter(label = "MIR source band",
-            description = "The mid-infrared band for the NDWI computation. If not provided," +
-                    " the operator will try to find the best fitting band.",
+    @Parameter(label = "Green source band",
+            description = "The green band for the NDWI2 computation. If not provided, the " +
+                    "operator will try to find the best fitting band.",
             rasterDataNodeType = Band.class)
-    private String mirSourceBand;
+    private String greenSourceBand;
 
     @Parameter(label = "NIR source band",
-            description = "The near-infrared band for the NDWI computation. If not provided," +
+            description = "The near-infrared band for the NDWI2 computation. If not provided," +
                     " the operator will try to find the best fitting band.",
             rasterDataNodeType = Band.class)
     private String nirSourceBand;
@@ -48,38 +48,38 @@ public class NdwiOp extends BaseIndexOp{
 
     @Override
     public void computeTileStack(Map<Band, Tile> targetTiles, Rectangle rectangle, ProgressMonitor pm) throws OperatorException {
-        pm.beginTask("Computing NDWI", rectangle.height);
+        pm.beginTask("Computing NDWI2", rectangle.height);
         try {
-            Tile mirTile = getSourceTile(getSourceProduct().getBand(mirSourceBand), rectangle);
+            Tile greenTile = getSourceTile(getSourceProduct().getBand(greenSourceBand), rectangle);
             Tile nirTile = getSourceTile(getSourceProduct().getBand(nirSourceBand), rectangle);
 
-            Tile ndwi = targetTiles.get(targetProduct.getBand(BAND_NAME));
-            Tile ndwiFlags = targetTiles.get(targetProduct.getBand(FLAGS_BAND_NAME));
+            Tile ndwi2 = targetTiles.get(targetProduct.getBand(BAND_NAME));
+            Tile ndwi2Flags = targetTiles.get(targetProduct.getBand(FLAGS_BAND_NAME));
 
-            float ndwiValue;
-            int ndwiFlagsValue;
+            float ndwi2Value;
+            int ndwi2FlagsValue;
 
             for (int y = rectangle.y; y < rectangle.y + rectangle.height; y++) {
                 for (int x = rectangle.x; x < rectangle.x + rectangle.width; x++) {
 
                     final float nir = nirFactor * nirTile.getSampleFloat(x, y);
-                    final float mir = mirFactor * mirTile.getSampleFloat(x, y);
+                    final float green = greenFactor * greenTile.getSampleFloat(x, y);
 
-                    ndwiValue = (nir - mir)/(nir + mir);
+                    ndwi2Value = (green - nir)/(green + nir);
 
-                    ndwiFlagsValue = 0;
-                    if (Float.isNaN(ndwiValue) || Float.isInfinite(ndwiValue)) {
-                        ndwiFlagsValue |= ARITHMETIC_FLAG_VALUE;
-                        ndwiValue = 0.0f;
+                    ndwi2FlagsValue = 0;
+                    if (Float.isNaN(ndwi2Value) || Float.isInfinite(ndwi2Value)) {
+                        ndwi2FlagsValue |= ARITHMETIC_FLAG_VALUE;
+                        ndwi2Value = 0.0f;
                     }
-                    if (ndwiValue < 0.0f) {
-                        ndwiFlagsValue |= LOW_FLAG_VALUE;
+                    if (ndwi2Value < 0.0f) {
+                        ndwi2FlagsValue |= LOW_FLAG_VALUE;
                     }
-                    if (ndwiValue > 1.0f) {
-                        ndwiFlagsValue |= HIGH_FLAG_VALUE;
+                    if (ndwi2Value > 1.0f) {
+                        ndwi2FlagsValue |= HIGH_FLAG_VALUE;
                     }
-                    ndwi.setSample(x, y, ndwiValue);
-                    ndwiFlags.setSample(x, y, ndwiFlagsValue);
+                    ndwi2.setSample(x, y, ndwi2Value);
+                    ndwi2Flags.setSample(x, y, ndwi2FlagsValue);
                 }
                 checkForCancellation();
                 pm.worked(1);
@@ -91,16 +91,16 @@ public class NdwiOp extends BaseIndexOp{
 
     @Override
     protected void loadSourceBands(Product product) throws OperatorException {
-        if (mirSourceBand == null) {
-            mirSourceBand = findBand(3000, 8000, product);
-            getLogger().info("Using band '" + mirSourceBand + "' as MIR input band.");
+        if (greenSourceBand == null) {
+            greenSourceBand = findBand(495, 570, product); /* (500, 590) */
+            getLogger().info("Using band '" + greenSourceBand + "' as GREEN input band.");
         }
         if (nirSourceBand == null) {
             nirSourceBand = findBand(800, 900, product);
             getLogger().info("Using band '" + nirSourceBand + "' as NIR input band.");
         }
-        if (mirSourceBand == null) {
-            throw new OperatorException("Unable to find band that could be used as mir input band. Please specify band.");
+        if (greenSourceBand == null) {
+            throw new OperatorException("Unable to find band that could be used as green input band. Please specify band.");
         }
         if (nirSourceBand == null) {
             throw new OperatorException("Unable to find band that could be used as nir input band. Please specify band.");
@@ -110,7 +110,7 @@ public class NdwiOp extends BaseIndexOp{
     public static class Spi extends OperatorSpi {
 
         public Spi() {
-            super(NdwiOp.class);
+            super(Ndwi2Op.class);
         }
 
     }
