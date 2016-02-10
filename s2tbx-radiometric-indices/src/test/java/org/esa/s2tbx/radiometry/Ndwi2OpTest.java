a2c54778786cd629c05147f64f8188c3f936e581
Dragos Mihailescu <dmihailescu@c-s.ro>
2016-02-10 12:15:37 +0200

    Add NDWI2 unit test.

---
+++ s2tbx-radiometric-indices/src/test/java/org/esa/s2tbx/radiometry/Ndwi2OpTest.java
@@ -0,0 +1,24 @@
+package org.esa.s2tbx.radiometry;
+
+import java.util.HashMap;
+
+/**
+ * Created by dmihailescu on 2/10/2016.
+ */
+
+public class Ndwi2OpTest extends BaseIndexOpTest<Ndwi2Op> {
+
+    @Override
+    public void setUp() throws Exception {
+        setupBands(new String[] { "GREEN", "NIR" }, 3, 3, new float[] { 550, 850 }, new float[] { 1, 2 }, new float[] { 9, 10 });
+        setOperatorParameters(new HashMap<String, Float>() {{
+            put("greenFactor", 1.0f);
+            put("nirFactor", 1.0f);
+        }});
+        setTargetValues(new float[] {
+                -0.333333f, -0.200000f, -0.142857f,
+                -0.111111f, -0.090909f, -0.076923f,
+                -0.066666f, -0.058823f, -0.052631f } );
+        super.setUp();
+    }
+}
