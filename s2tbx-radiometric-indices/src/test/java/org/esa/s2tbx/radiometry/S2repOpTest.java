e77c8f17813c74648ac131dc61687b74116aff34
Dragos Mihailescu <dmihailescu@c-s.ro>
2016-02-10 18:57:07 +0200

    Add REIP and S2REP indices unit tests.

---
+++ s2tbx-radiometric-indices/src/test/java/org/esa/s2tbx/radiometry/S2repOpTest.java
@@ -0,0 +1,26 @@
+package org.esa.s2tbx.radiometry;
+
+import java.util.HashMap;
+
+/**
+ * Created by dmihailescu on 2/10/2016.
+ */
+
+public class S2repOpTest extends BaseIndexOpTest<S2repOp> {
+
+    @Override
+    public void setUp() throws Exception {
+        setupBands(new String[] { "RED (B4)", "RED (B5)", "RED (B6)", "NIR (B7)" }, 3, 3, new float[] { 665, 705, 740, 783 }, new float[] { 1, 2, 3, 4 }, new float[] { 9, 10, 11, 12 });
+        setOperatorParameters(new HashMap<String, Float>() {{
+            put("redB4Factor", 1.0f);
+            put("redB5Factor", 1.0f);
+            put("redB6Factor", 1.0f);
+            put("nirFactor", 1.0f);
+        }});
+        setTargetValues(new float[] {
+                722.5f, 722.5f, 722.5f,
+                722.5f, 722.5f, 722.5f,
+                722.5f, 722.5f, 722.5f } );
+        super.setUp();
+    }
+}
