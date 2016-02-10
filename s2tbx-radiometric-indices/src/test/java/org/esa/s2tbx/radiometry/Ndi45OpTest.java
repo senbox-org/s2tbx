f106b41ed5ad62ef68b72e0384b36a513577a337
Dragos Mihailescu <dmihailescu@c-s.ro>
2016-02-10 11:46:18 +0200

    Add NDI45 unit test.

---
+++ s2tbx-radiometric-indices/src/test/java/org/esa/s2tbx/radiometry/Ndi45OpTest.java
@@ -0,0 +1,24 @@
+package org.esa.s2tbx.radiometry;
+
+import java.util.HashMap;
+
+/**
+ * Created by dmihailescu on 2/10/2016.
+ */
+
+public class Ndi45OpTest extends BaseIndexOpTest<Ndi45Op> {
+
+    @Override
+    public void setUp() throws Exception {
+        setupBands(new String[] { "RED", "NIR" }, 3, 3, new float[] { 665, 705 }, new float[] { 1, 2 }, new float[] { 9, 10 });
+        setOperatorParameters(new HashMap<String, Float>() {{
+            put("redFactor", 1.0f);
+            put("nirFactor", 1.0f);
+        }});
+        setTargetValues(new float[] {
+                0.333333f, 0.200000f, 0.142857f,
+                0.111111f, 0.090909f, 0.076923f,
+                0.066666f, 0.058823f, 0.052631f } );
+        super.setUp();
+    }
+}
