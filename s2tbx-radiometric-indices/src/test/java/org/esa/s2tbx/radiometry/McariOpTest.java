0adca4a5dc66bbae2c05bddb914b99762c175c3e
Dragos Mihailescu <dmihailescu@c-s.ro>
2016-02-10 18:32:26 +0200

    Add MCARI index unit test.

---
+++ s2tbx-radiometric-indices/src/test/java/org/esa/s2tbx/radiometry/McariOpTest.java
@@ -0,0 +1,25 @@
+package org.esa.s2tbx.radiometry;
+
+import java.util.HashMap;
+
+/**
+ * Created by dmihailescu on 2/10/2016.
+ */
+
+public class McariOpTest extends BaseIndexOpTest<McariOp> {
+
+    @Override
+    public void setUp() throws Exception {
+        setupBands(new String[] { "GREEN (B3)", "RED (B4)", "RED (B5)" }, 3, 3, new float[] { 560, 665, 705 }, new float[] { 1, 2, 3 }, new float[] { 9, 10, 11 });
+        setOperatorParameters(new HashMap<String, Float>() {{
+            put("greenFactor", 1.0f);
+            put("redB4Factor", 1.0f);
+            put("redB5Factor", 1.0f);
+        }});
+        setTargetValues(new float[] {
+                0.6f, 0.6f, 0.6f,
+                0.6f, 0.6f, 0.6f,
+                0.6f, 0.6f, 0.6f } );
+        super.setUp();
+    }
+}
