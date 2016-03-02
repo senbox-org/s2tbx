77386a3782adb9dff1882c8b1e48b0e90b48e198
Ramona Manda <ramona.manda@c-s.ro>
2016-03-02 16:30:23 +0200

    added unit tests for JP2 Reader

---
+++ s2tbx-jp2-reader/src/test/java/org/esa/s2tbx/dataio/jp2/JP2productReaderAcceptanceTest.java
@@ -0,0 +1,9 @@
+package org.esa.s2tbx.dataio.jp2;
+
+
+//import org.esa.snap.dataio.ProductReaderAcceptanceTest;
+
+public class JP2ProductReaderAcceptanceTest //extends ProductReaderAcceptanceTest
+{
+
+}
