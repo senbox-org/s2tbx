77be9c6dc9717b4d94d5a079076efb31b604a30e
Nicolas Ducoin <nicolas.ducoin@c-s.fr>
2016-07-22 12:11:25 +0200

    Changed version to 5.0.0

--- s2tbx-sta-adapters/orfeotoolbox/maintenance/ToolAdapterGenerator.py
+++ s2tbx-sta-adapters/orfeotoolbox/maintenance/ToolAdapterGenerator.py
@@ -717,7 +717,7 @@ def run_ToolAdapterGenerator(outputDir, xmlDescriptionProcessing, createAdapter=
 
     if createAdapter:
         createPom(toolDirectory, dicResult, lowerToolName)
-        createManifest(metaInfDirectory, dicResult, "4.0.1")
+        createManifest(metaInfDirectory, dicResult, "5.0.0")
 
 
 
