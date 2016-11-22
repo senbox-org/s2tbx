e5721bffe9e1409ed0eb60c7f3adab02d49732ec
Cosmin Cara <kraftek@c-s.ro>
2016-11-22 17:46:00 +0200

    Docs and copyright update

--- s2tbx-radiometric-indices-ui/src/main/java/org/esa/s2tbx/radiometry/RadiometricOperatorAction.java
+++ s2tbx-radiometric-indices-ui/src/main/java/org/esa/s2tbx/radiometry/RadiometricOperatorAction.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  * Copyright (C) 2016 CS ROMANIA
+ *  *
+ *  * This program is free software; you can redistribute it and/or modify it
+ *  * under the terms of the GNU General Public License as published by the Free
+ *  * Software Foundation; either version 3 of the License, or (at your option)
+ *  * any later version.
+ *  * This program is distributed in the hope that it will be useful, but WITHOUT
+ *  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *  * more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License along
+ *  *  with this program; if not, see http://www.gnu.org/licenses/
+ *
+ */
+
 package org.esa.s2tbx.radiometry;
 
 import org.esa.snap.core.gpf.ui.DefaultOperatorAction;
@@ -10,7 +28,9 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * @author Jean Coravu.
+ * Generic action for radiometric indices.
+ *
+ * @author  Jean Coravu
  */
 public class RadiometricOperatorAction extends DefaultOperatorAction {
     private static final Set<String> KNOWN_KEYS = new HashSet<>(Arrays.asList("displayName", "operatorName", "dialogTitle", "helpId", "targetProductNameSuffix"));
--- s2tbx-radiometric-indices/src/main/java/org/esa/s2tbx/radiometry/BaseIndexOp.java
+++ s2tbx-radiometric-indices/src/main/java/org/esa/s2tbx/radiometry/BaseIndexOp.java
@@ -1,3 +1,21 @@
+/*
+ *
+ *  * Copyright (C) 2016 CS ROMANIA
+ *  *
+ *  * This program is free software; you can redistribute it and/or modify it
+ *  * under the terms of the GNU General Public License as published by the Free
+ *  * Software Foundation; either version 3 of the License, or (at your option)
+ *  * any later version.
+ *  * This program is distributed in the hope that it will be useful, but WITHOUT
+ *  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *  * more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License along
+ *  *  with this program; if not, see http://www.gnu.org/licenses/
+ *
+ */
+
 package org.esa.s2tbx.radiometry;
 
 import org.esa.snap.core.datamodel.*;
@@ -48,13 +66,13 @@ public abstract class BaseIndexOp extends Operator {
             defaultValue = RESAMPLE_NONE, valueSet = { RESAMPLE_NONE, RESAMPLE_LOWEST, RESAMPLE_HIGHEST })
     protected String resampleType;
     @Parameter(alias = "upsampling",
-            label = "Upsampling method",
+            label = "Upsampling Method",
             description = "The method used for interpolation (upsampling to a finer resolution).",
             valueSet = {"Nearest", "Bilinear", "Bicubic"},
             defaultValue = "Nearest")
     protected String upsamplingMethod;
     @Parameter(alias = "downsampling",
-            label = "Downsampling method",
+            label = "Downsampling Method",
             description = "The method used for aggregation (downsampling to a coarser resolution).",
             valueSet = {"First", "Min", "Max", "Mean", "Median"},
             defaultValue = "First")
