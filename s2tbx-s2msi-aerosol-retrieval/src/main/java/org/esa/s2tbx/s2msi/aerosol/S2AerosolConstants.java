21461aa110548132ea7c4bee6f843f6795805d2e
Olaf Danne <olaf.danne@brockmann-consult.de>
2017-01-12 18:10:40 +0100

    continued S2 aerosol following GK advice

---
+++ s2tbx-s2msi-aerosol-retrieval/src/main/java/org/esa/s2tbx/s2msi/aerosol/S2AerosolConstants.java
@@ -0,0 +1,13 @@
+package org.esa.s2tbx.s2msi.aerosol;
+
+/**
+ * todo: add comment
+ * To change this template use File | Settings | File Templates.
+ * Date: 12.01.2017
+ * Time: 15:44
+ *
+ * @author olafd
+ */
+public class S2AerosolConstants {
+    public static final double RHO_REFERENCE = 0.15;
+}
