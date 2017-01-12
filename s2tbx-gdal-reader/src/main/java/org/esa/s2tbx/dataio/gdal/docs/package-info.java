edb83939aa2ed35c4fc7d9a734e55820678f23b9
ramonamanda <ramona.manda@c-s.ro>
2017-01-12 17:50:16 +0200

    Show the GDAL documentation in the Help.

---
+++ s2tbx-gdal-reader/src/main/java/org/esa/s2tbx/dataio/gdal/docs/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * author Jean Coravu
+ */
+@HelpSetRegistration(helpSet = "help.hs", position = 2332)
+package org.esa.s2tbx.dataio.gdal.docs;
+
+import org.netbeans.api.javahelp.HelpSetRegistration;
