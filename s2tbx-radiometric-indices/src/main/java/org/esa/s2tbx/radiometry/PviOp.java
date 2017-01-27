366f582a9511f6e9610bde955f110c7dccf74eb6
ramonam <ramona.manda@c-s.ro>
2017-01-27 11:15:12 +0200

    Fix for a test fail

--- s2tbx-radiometric-indices/src/main/java/org/esa/s2tbx/radiometry/PviOp.java
+++ s2tbx-radiometric-indices/src/main/java/org/esa/s2tbx/radiometry/PviOp.java
@@ -19,6 +19,7 @@
 package org.esa.s2tbx.radiometry;
 
 import com.bc.ceres.core.ProgressMonitor;
+import org.esa.s2tbx.radiometry.annotations.BandParameter;
 import org.esa.snap.core.datamodel.Band;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.gpf.OperatorException;
@@ -55,12 +56,14 @@ public class PviOp extends BaseIndexOp{
             description = "The red band for the PVI computation. If not provided, the " +
                     "operator will try to find the best fitting band.",
             rasterDataNodeType = Band.class)
+    @BandParameter(minWavelength = 600, maxWavelength = 650)
     private String redSourceBand;
 
     @Parameter(label = "NIR source band",
             description = "The near-infrared band for the PVI computation. If not provided," +
                     " the operator will try to find the best fitting band.",
             rasterDataNodeType = Band.class)
+    @BandParameter(minWavelength = 800, maxWavelength = 900)
     private String nirSourceBand;
 
     @Override
@@ -127,6 +130,7 @@ public class PviOp extends BaseIndexOp{
         if (nirSourceBand == null) {
             throw new OperatorException("Unable to find band that could be used as nir input band. Please specify band.");
         }
+        this.sourceBandNames = new String[] { redSourceBand, nirSourceBand };
     }
 
 
