e3d46477c3409552d5edf92ba505c1480ec97f33
Cosmin Cara <kraftek@c-s.ro>
2017-04-21 19:46:52 +0300

    STA changes: - lookup variables in cluster properties - corrections for menu action registration

--- lib-gdal/src/main/java/org/esa/s2tbx/dataio/gdal/GdalInstallInfo.java
+++ lib-gdal/src/main/java/org/esa/s2tbx/dataio/gdal/GdalInstallInfo.java
@@ -1,7 +1,11 @@
 package org.esa.s2tbx.dataio.gdal;
 
+import org.esa.snap.runtime.Config;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
 
 /**
  * Created by kraftek on 11/15/2016.
@@ -14,6 +18,19 @@ public enum GdalInstallInfo {
 
     public synchronized void setLocations(Path binLocation) {
         this.binLocation = binLocation;
+        try {
+            Config config = Config.instance("s2tbx");
+            config.load();
+            Preferences preferences = config.preferences();
+            preferences.put("gdal.apps.path",
+                            this.binLocation.resolve("bin")
+                                            .resolve("gdal")
+                                            .resolve("apps")
+                                            .toString());
+            preferences.flush();
+        } catch (BackingStoreException exception) {
+            // ignore exception
+        }
         fireListener();
     }
 
@@ -32,4 +49,6 @@ public enum GdalInstallInfo {
     public boolean isPresent() {
         return this.binLocation != null && Files.exists(this.binLocation);
     }
+
+    public Path getLocation() { return this.binLocation; }
 }
