5f38ef9166075aeebe4d1c3e66eadf3e71848c62
Cosmin Cara <kraftek@c-s.ro>
2017-05-24 19:48:11 +0300

    Added support for using CCI Land Cover 2015

---
+++ s2tbx-land-cover/src/main/java/org/esa/s2tbx/landcover/dataio/CCILandCoverModelDescriptor.java
@@ -0,0 +1,66 @@
+/*
+ *
+ *  * Copyright (C) 2016 CS ROMANIA
+ *  *
+ *  * This program is free software; you can redistribute it and/or modify it
+ *  * under the terms of the GNU General Public License as published by the Free
+ *  * Software Foundation; either version 3 of the License, or (at your option)
+ *  * any later version.
+ *  * This program is distributed in the hope that it will be useful, but WITHOUT
+ *  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ *  * more details.
+ *  *
+ *  * You should have received a copy of the GNU General Public License along
+ *  *  with this program; if not, see http://www.gnu.org/licenses/
+ *
+ */
+
+package org.esa.s2tbx.landcover.dataio;
+
+import org.esa.snap.core.dataop.resamp.Resampling;
+import org.esa.snap.core.util.ResourceInstaller;
+import org.esa.snap.engine_utilities.util.Settings;
+import org.esa.snap.landcover.dataio.AbstractLandCoverModelDescriptor;
+import org.esa.snap.landcover.dataio.FileLandCoverModel;
+import org.esa.snap.landcover.dataio.LandCoverModel;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+
+/**
+ * @author Cosmin Cara
+ */
+public class CCILandCoverModelDescriptor extends AbstractLandCoverModelDescriptor {
+    public static final String NAME = "CCILandCover-2015";
+
+    private static final File INSTALL_DIR = new File(Settings.instance().getAuxDataFolder().getAbsolutePath(),
+                                                     "LandCover" + File.separator + NAME);
+    private static final File[] fileList = new File[] {
+            new File(INSTALL_DIR, "ESACCI-LC-L4-LCCS-Map-300m-P1Y-2015-v2.0.7.zip")
+    };
+
+    public CCILandCoverModelDescriptor() {
+        remotePath = "https://storage.googleapis.com/cci-lc-v207/";
+        name = NAME;
+        NO_DATA_VALUE = 0;
+        installDir = INSTALL_DIR;
+
+        final Path moduleBasePath = ResourceInstaller.findModuleCodeBasePath(this.getClass());
+        colourIndexFile = moduleBasePath.resolve("org/esa/snap/landcover/auxdata/ccilandcover_index.col");
+    }
+
+    @Override
+    public LandCoverModel createLandCoverModel(Resampling resampling) throws IOException {
+        return new FileLandCoverModel(this, fileList, resampling);
+    }
+
+    @Override
+    public String createTileFilename(int tileX, int tileY) {
+        return fileList[0].getName();
+    }
+
+    @Override
+    public boolean isInstalled() { return true; }
+}
