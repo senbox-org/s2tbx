988ea304a1a944e96fe34c7a9e50499c2e52f405
ramonamanda <ramona.monda@c-s.ro>
2017-05-31 18:32:47 +0300

    added prameter group annotation class , minor modifications to forect cover change GUI module

--- s2tbx-forest-cover-change-ui/src/main/java/org/esa/s2tbx/fcc/FccDialog.java
+++ s2tbx-forest-cover-change-ui/src/main/java/org/esa/s2tbx/fcc/FccDialog.java
@@ -1,84 +1,395 @@
 package org.esa.s2tbx.fcc;
 
+import com.bc.ceres.binding.Property;
+import com.bc.ceres.binding.PropertyContainer;
+import com.bc.ceres.binding.PropertyDescriptor;
 import com.bc.ceres.binding.PropertySet;
+import com.bc.ceres.binding.ValidationException;
+import com.bc.ceres.binding.ValueSet;
+import com.bc.ceres.binding.accessors.MapEntryAccessor;
+import com.bc.ceres.swing.TableLayout;
 import com.bc.ceres.swing.binding.BindingContext;
+import com.bc.ceres.swing.binding.PropertyPane;
+import com.bc.ceres.swing.selection.AbstractSelectionChangeListener;
+import com.bc.ceres.swing.selection.Selection;
 import com.bc.ceres.swing.selection.SelectionChangeEvent;
-import com.bc.ceres.swing.selection.SelectionChangeListener;
+import java.beans.PropertyChangeListener;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.border.EmptyBorder;
 import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.datamodel.ProductNodeEvent;
+import org.esa.snap.core.datamodel.ProductNodeListener;
+import org.esa.snap.core.datamodel.RasterDataNode;
+import org.esa.snap.core.gpf.GPF;
+import org.esa.snap.core.gpf.Operator;
+import org.esa.snap.core.gpf.OperatorSpi;
+import org.esa.snap.core.gpf.annotations.Parameter;
+import org.esa.snap.core.gpf.annotations.ParameterDescriptorFactory;
+import org.esa.snap.core.gpf.annotations.SourceProduct;
+import org.esa.snap.core.gpf.annotations.SourceProducts;
+import org.esa.snap.core.gpf.annotations.TargetProduct;
+import org.esa.snap.core.gpf.annotations.TargetProperty;
+import org.esa.snap.core.gpf.descriptor.AnnotationParameterDescriptor;
+import org.esa.snap.core.gpf.descriptor.AnnotationSourceProductDescriptor;
+import org.esa.snap.core.gpf.descriptor.AnnotationSourceProductsDescriptor;
+import org.esa.snap.core.gpf.descriptor.AnnotationTargetProductDescriptor;
+import org.esa.snap.core.gpf.descriptor.AnnotationTargetPropertyDescriptor;
+import org.esa.snap.core.gpf.descriptor.OperatorDescriptor;
+import org.esa.snap.core.gpf.descriptor.ParameterDescriptor;
+import org.esa.snap.core.gpf.descriptor.SourceProductDescriptor;
+import org.esa.snap.core.gpf.descriptor.SourceProductsDescriptor;
+import org.esa.snap.core.gpf.descriptor.TargetProductDescriptor;
+import org.esa.snap.core.gpf.descriptor.TargetPropertyDescriptor;
+import org.esa.snap.core.gpf.internal.RasterDataNodeValues;
 import org.esa.snap.core.gpf.ui.DefaultIOParametersPanel;
-import org.esa.snap.core.gpf.ui.DefaultSingleTargetProductDialog;
+import org.esa.snap.core.gpf.ui.OperatorMenu;
+import org.esa.snap.core.gpf.ui.OperatorParameterSupport;
+import org.esa.snap.core.gpf.ui.SingleTargetProductDialog;
 import org.esa.snap.core.gpf.ui.SourceProductSelector;
+import org.esa.snap.core.gpf.ui.TargetProductSelectorModel;
 import org.esa.snap.ui.AppContext;
-import org.esa.snap.utils.StringHelper;
-
+import org.esa.snap.ui.UIUtils;
+import org.esa.s2tbx.fcc.annotation.*;
 /**
  * @author Razvan Dumitrascu
  * @since 5.0.6
  */
-public class FccDialog extends DefaultSingleTargetProductDialog {
+public class FccDialog extends SingleTargetProductDialog {
 
+    private final String operatorName;
+    private final OperatorDescriptor operatorDescriptor;
+    private DefaultIOParametersPanel ioParametersPanel;
+    private final OperatorParameterSupport parameterSupport;
+    private final BindingContext bindingContext;
+    private List<ParameterDescriptor> parameterDescriptors;
+    private List<SourceProductDescriptor> sourceProductDescriptors;
+    private TargetProductDescriptor targetProductDescriptor;
+    private List<TargetPropertyDescriptor> targetPropertyDescriptors;
+    private SourceProductsDescriptor sourceProductsDescriptor;
+    private Map<String, List<String>> parameterGroupDescriptors;
+    private JTabbedPane form;
+    private PropertyDescriptor[] rasterDataNodeTypeProperties;
+    private String targetProductNameSuffix;
+    private ProductChangedHandler productChangedHandler;
 
     public FccDialog(String operatorName, AppContext appContext, String title, String helpID) {
         this(operatorName, appContext, title, helpID, true);
     }
 
     public FccDialog(String operatorName, AppContext appContext, String title, String helpID, boolean targetProductSelectorDisplay) {
-        super(operatorName, appContext, title, helpID, targetProductSelectorDisplay);
+        super(appContext, title, ID_APPLY_CLOSE, helpID);
+        this.operatorName = operatorName;
+        targetProductNameSuffix = "";
+        processAnnotationsRec(ForestCoverChangeOp.class);
+        OperatorSpi operatorSpi = GPF.getDefaultInstance().getOperatorSpiRegistry().getOperatorSpi(operatorName);
+        if (operatorSpi == null) {
+            throw new IllegalArgumentException("No SPI found for operator name '" + operatorName + "'");
+        }
 
-        DefaultIOParametersPanel ioParametersPanel = getDefaultIOParametersPanel();
-        List<SourceProductSelector> sourceProductSelectorList = ioParametersPanel.getSourceProductSelectorList();
+        operatorDescriptor = operatorSpi.getOperatorDescriptor();
+        ioParametersPanel = new DefaultIOParametersPanel(getAppContext(), operatorDescriptor, getTargetProductSelector(), targetProductSelectorDisplay);
+
+        parameterSupport = new OperatorParameterSupport(operatorDescriptor);
+        final ArrayList<SourceProductSelector> sourceProductSelectorList = ioParametersPanel.getSourceProductSelectorList();
+        final PropertySet propertySet = parameterSupport.getPropertySet();
+
+        bindingContext = new BindingContext(propertySet);
+
+        if (propertySet.getProperties().length > 0) {
+            if (!sourceProductSelectorList.isEmpty()) {
+                Property[] properties = propertySet.getProperties();
+                List<PropertyDescriptor> rdnTypeProperties = new ArrayList<>(properties.length);
+                for (Property property : properties) {
+                    PropertyDescriptor parameterDescriptor = property.getDescriptor();
+                    if (parameterDescriptor.getAttribute(RasterDataNodeValues.ATTRIBUTE_NAME) != null) {
+                        rdnTypeProperties.add(parameterDescriptor);
+                    }
+                }
+                rasterDataNodeTypeProperties = rdnTypeProperties.toArray(
+                        new PropertyDescriptor[rdnTypeProperties.size()]);
+            }
+        }
+        productChangedHandler = new ProductChangedHandler();
         if (!sourceProductSelectorList.isEmpty()) {
-            SelectionChangeListener listener = new SelectionChangeListener() {
-                public void selectionChanged(SelectionChangeEvent event) {
-                    processSelectedProduct();
-                }
-
-                public void selectionContextChanged(SelectionChangeEvent event) {
-                }
-            };
-            sourceProductSelectorList.get(0).addSelectionChangeListener(listener);
+            sourceProductSelectorList.get(0).addSelectionChangeListener(productChangedHandler);
         }
     }
 
     @Override
     public int show() {
-        int result = super.show();
-        processSelectedProduct();
-        return result;
+        ioParametersPanel.initSourceProductSelectors();
+        if (form == null) {
+            initForm();
+            if (getJDialog().getJMenuBar() == null) {
+                final OperatorMenu operatorMenu = createDefaultMenuBar();
+                getJDialog().setJMenuBar(operatorMenu.createDefaultMenu());
+            }
+        }
+        setContent(form);
+        return super.show();
     }
 
     @Override
     public void hide() {
+        productChangedHandler.releaseProduct();
+        ioParametersPanel.releaseSourceProductSelectors();
         super.hide();
     }
 
-
-    /**
-     * Returns the selected product.
-     *
-     * @return the selected product
-     */
-    private Product getSelectedProduct() {
-        DefaultIOParametersPanel ioParametersPanel = getDefaultIOParametersPanel();
-        List<SourceProductSelector> sourceProductSelectorList = ioParametersPanel.getSourceProductSelectorList();
-        return sourceProductSelectorList.get(0).getSelectedProduct();
+    @Override
+    protected Product createTargetProduct() throws Exception {
+        final HashMap<String, Product> sourceProducts = ioParametersPanel.createSourceProductsMap();
+        return GPF.createProduct(operatorName, parameterSupport.getParameterMap(), sourceProducts);
     }
 
-    /**
-     * Sets the incidence angle and the quantification value according to the selected product.
-     */
-    private void processSelectedProduct() {
-        Product selectedProduct = getSelectedProduct();
-        if (selectedProduct != null) {
-            boolean isSentinel2 = StringHelper.startsWithIgnoreCase(selectedProduct.getProductType(), "S2_MSI_Level");
-            BindingContext bindingContext = getBindingContext();
-            PropertySet propertySet = bindingContext.getPropertySet();
-            propertySet.setDefaultValues();
-            String propertyName = "percentage";
-            if (bindingContext.getBinding(propertyName) != null) {
+    protected DefaultIOParametersPanel getDefaultIOParametersPanel() {
+        return ioParametersPanel;
+    }
 
+    public String getTargetProductNameSuffix() {
+        return targetProductNameSuffix;
+    }
+
+    public void setTargetProductNameSuffix(String suffix) {
+        targetProductNameSuffix = suffix;
+    }
+
+    public BindingContext getBindingContext() {
+        return bindingContext;
+    }
+
+    private void initForm() {
+        form = new JTabbedPane();
+        form.add("I/O Parameters", ioParametersPanel);
+        final TableLayout layout = new TableLayout(1);
+        layout.setTableAnchor(TableLayout.Anchor.WEST);
+        layout.setTableFill(TableLayout.Fill.BOTH);
+        layout.setTableWeightX(1.0);
+        layout.setTableWeightY(0.0);
+        layout.setRowWeightY(2, 1.0);
+        layout.setTablePadding(3, 3);
+
+
+        if (bindingContext.getPropertySet().getProperties().length > 0) {
+            PropertyContainer container = new PropertyContainer();
+            container.addProperties(bindingContext.getPropertySet().getProperties());
+            for(Map.Entry<String, List<String>> pair : parameterGroupDescriptors.entrySet())
+            {
+                for(String prop:pair.getValue()){
+                    container.removeProperty(bindingContext.getPropertySet().getProperty(prop));
+                }
+            }
+            final PropertyPane parametersPane = new PropertyPane(container);
+            final JPanel parametersPanel = new JPanel(layout);
+            parametersPanel.add(parametersPane.createPanel());
+            parametersPanel.setBorder(new EmptyBorder(4, 4, 4, 4));
+            form.add("Processing Parameters", new JScrollPane(parametersPanel));
+            for(Map.Entry<String, List<String>> pair : parameterGroupDescriptors.entrySet())
+            {
+                parametersPanel.add(createPanel(pair.getKey()+ " parameters", bindingContext, pair.getValue()));
+            }
+            updateSourceProduct();
+        }
+    }
+
+    private JPanel createPanel(String name, BindingContext bindingContext, List<String> parameters){
+
+        PropertyContainer container = new PropertyContainer();
+        for(String parameter: parameters){
+            Property prop = bindingContext.getPropertySet().getProperty(parameter);
+            container.addProperty(prop);
+        }
+        final PropertyPane parametersPane = new PropertyPane(container);
+        final JPanel panel = parametersPane.createPanel();
+        panel.setBorder(BorderFactory.createTitledBorder(name));
+        return panel;
+    }
+
+    private void processAnnotationsRec(Class<?> operatorClass) {
+
+        final Class<?> superclass = operatorClass.getSuperclass();
+        if (superclass != null && !superclass.equals(Operator.class)) {
+            processAnnotationsRec(superclass);
+        }
+
+        final Field[] declaredFields = operatorClass.getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+
+            String fieldName = declaredField.getName();
+            Class<?> fieldType = declaredField.getType();
+
+            ParameterGroup parameterGroupAnnotation = declaredField.getAnnotation(ParameterGroup.class);
+            if(parameterGroupAnnotation!=null){
+                String alias = parameterGroupAnnotation.alias();
+                if (parameterGroupDescriptors == null) {
+                    parameterGroupDescriptors = new HashMap<>();
+                }
+                List<String>value = parameterGroupDescriptors.get(alias);
+                if(value == null){
+                    value = new ArrayList<>();
+                    parameterGroupDescriptors.put(alias, value);
+                }
+                value.add(fieldName);
+                continue;
+            }
+            Parameter parameterAnnotation = declaredField.getAnnotation(Parameter.class);
+            if (parameterAnnotation != null) {
+                if (parameterDescriptors == null) {
+                    parameterDescriptors = new ArrayList<>();
+                }
+                boolean isDeprecated = declaredField.getAnnotation(Deprecated.class) != null;
+                parameterDescriptors.add(new AnnotationParameterDescriptor(fieldName, fieldType, isDeprecated, parameterAnnotation));
+                continue;
+            }
+
+            SourceProduct sourceProductAnnotation = declaredField.getAnnotation(SourceProduct.class);
+            if (sourceProductAnnotation != null && Product.class.isAssignableFrom(fieldType)) {
+                if (sourceProductDescriptors == null) {
+                    sourceProductDescriptors = new ArrayList<>();
+                }
+                sourceProductDescriptors.add(new AnnotationSourceProductDescriptor(fieldName, sourceProductAnnotation));
+                continue;
+            }
+
+            SourceProducts sourceProductsAnnotation = declaredField.getAnnotation(SourceProducts.class);
+            if (sourceProductsAnnotation != null && Product[].class.isAssignableFrom(fieldType)) {
+                // Note: superclass declarations are overwritten here.
+                sourceProductsDescriptor = new AnnotationSourceProductsDescriptor(fieldName, sourceProductsAnnotation);
+                continue;
+            }
+
+            TargetProduct targetProductAnnotation = declaredField.getAnnotation(TargetProduct.class);
+            if (targetProductAnnotation != null) {
+                // Note: superclass declarations are overwritten here.
+                targetProductDescriptor = new AnnotationTargetProductDescriptor(fieldName, targetProductAnnotation);
+                continue;
+            }
+
+            TargetProperty targetPropertyAnnotation = declaredField.getAnnotation(TargetProperty.class);
+            if (targetPropertyAnnotation != null) {
+                if (targetPropertyDescriptors == null) {
+                    targetPropertyDescriptors = new ArrayList<>();
+                }
+                targetPropertyDescriptors.add(new AnnotationTargetPropertyDescriptor(fieldName, fieldType, targetPropertyAnnotation));
             }
         }
+    }
+
+    private OperatorMenu createDefaultMenuBar() {
+        return new OperatorMenu(getJDialog(),
+                operatorDescriptor,
+                parameterSupport,
+                getAppContext(),
+                getHelpID());
+    }
+
+    private void updateSourceProduct() {
+        try {
+            Property property = bindingContext.getPropertySet().getProperty(UIUtils.PROPERTY_SOURCE_PRODUCT);
+            if (property != null) {
+                property.setValue(productChangedHandler.currentProduct);
+            }
+        } catch (ValidationException e) {
+            throw new IllegalStateException("Property '" + UIUtils.PROPERTY_SOURCE_PRODUCT + "' must be of type " + Product.class + ".", e);
+        }
+    }
+
+    private class ProductChangedHandler extends AbstractSelectionChangeListener implements ProductNodeListener {
+
+        private Product currentProduct;
+
+        public void releaseProduct() {
+            if (currentProduct != null) {
+                currentProduct.removeProductNodeListener(this);
+                currentProduct = null;
+                updateSourceProduct();
+            }
+        }
+
+        @Override
+        public void selectionChanged(SelectionChangeEvent event) {
+            Selection selection = event.getSelection();
+            if (selection != null) {
+                final Product selectedProduct = (Product) selection.getSelectedValue();
+                if (selectedProduct != currentProduct) {
+                    if (currentProduct != null) {
+                        currentProduct.removeProductNodeListener(this);
+                    }
+                    currentProduct = selectedProduct;
+                    if (currentProduct != null) {
+                        currentProduct.addProductNodeListener(this);
+                    }
+                    if(getTargetProductSelector() != null){
+                        updateTargetProductName();
+                    }
+                    updateValueSets(currentProduct);
+                    updateSourceProduct();
+                }
+            }
+        }
+
+        @Override
+        public void nodeAdded(ProductNodeEvent event) {
+            handleProductNodeEvent();
+        }
+
+        @Override
+        public void nodeChanged(ProductNodeEvent event) {
+            handleProductNodeEvent();
+        }
+
+        @Override
+        public void nodeDataChanged(ProductNodeEvent event) {
+            handleProductNodeEvent();
+        }
+
+        @Override
+        public void nodeRemoved(ProductNodeEvent event) {
+            handleProductNodeEvent();
+        }
+
+        private void updateTargetProductName() {
+            String productName = "";
+            if (currentProduct != null) {
+                productName = currentProduct.getName();
+            }
+            final TargetProductSelectorModel targetProductSelectorModel = getTargetProductSelector().getModel();
+            targetProductSelectorModel.setProductName(productName + getTargetProductNameSuffix());
+        }
+
+        private void handleProductNodeEvent() {
+            updateValueSets(currentProduct);
+        }
+
+        private void updateValueSets(Product product) {
+            if (rasterDataNodeTypeProperties != null) {
+                for (PropertyDescriptor propertyDescriptor : rasterDataNodeTypeProperties) {
+                    updateValueSet(propertyDescriptor, product);
+                }
+            }
+        }
+    }
+
+    private static void updateValueSet(PropertyDescriptor propertyDescriptor, Product product) {
+        String[] values = new String[0];
+        if (product != null) {
+            Object object = propertyDescriptor.getAttribute(RasterDataNodeValues.ATTRIBUTE_NAME);
+            if (object != null) {
+                @SuppressWarnings("unchecked")
+                Class<? extends RasterDataNode> rasterDataNodeType = (Class<? extends RasterDataNode>) object;
+                boolean includeEmptyValue = !propertyDescriptor.isNotNull() && !propertyDescriptor.isNotEmpty() &&
+                        !propertyDescriptor.getType().isArray();
+                values = RasterDataNodeValues.getNames(product, rasterDataNodeType, includeEmptyValue);
+            }
+        }
+        propertyDescriptor.setValueSet(new ValueSet(values));
     }
 }
--- s2tbx-forest-cover-change/src/main/java/org/esa/s2tbx/fcc/ForestCoverChangeOP.java
+++ s2tbx-forest-cover-change/src/main/java/org/esa/s2tbx/fcc/ForestCoverChangeOP.java
@@ -1,5 +1,6 @@
 package org.esa.s2tbx.fcc;
 
+import org.esa.s2tbx.fcc.annotation.ParameterGroup;
 import org.esa.snap.core.datamodel.Product;
 import org.esa.snap.core.gpf.Operator;
 import org.esa.snap.core.gpf.OperatorException;
@@ -25,6 +26,12 @@ public class ForestCoverChangeOp extends Operator{
 
     @SuppressWarnings({"PackageVisibleField"})
 
+    public static final String SPRING_MERGING_COST_CRITERION = "Spring";
+    public static final String BAATZ_SCHAPE_MERGING_COST_CRITERION = "Baatz & Schape";
+    public static final String FULL_LANDA_SCHEDULE_MERGING_COST_CRITERION = "Full Lamda Schedule";
+    public static final String BEST_FITTING_REGION_MERGING_CRITERION = "Best Fitting";
+    public static final String LOCAL_MUTUAL_BEST_FITTING_REGION_MERGING_CRITERION = "Local Mutual Best Fitting";
+
     @SourceProduct(alias = "Source Product TM", description = "The source product to be modified.")
     private Product sourceProductTM;
     @SourceProduct(alias = "Source Product ETM", description = "The source product to be modified.")
@@ -35,6 +42,39 @@ public class ForestCoverChangeOp extends Operator{
 
     @Parameter(defaultValue = "95.0", itemAlias = "percentage", description = "Specifies the percentage of forest cover per segment")
     private float percentage;
+
+    @ParameterGroup(alias = "Segmentation")
+    @Parameter(label = "Merging cost criterion",
+            defaultValue = BAATZ_SCHAPE_MERGING_COST_CRITERION,
+            description = "The method to compute the region merging.",
+            valueSet = {SPRING_MERGING_COST_CRITERION, BAATZ_SCHAPE_MERGING_COST_CRITERION, FULL_LANDA_SCHEDULE_MERGING_COST_CRITERION})
+    private String mergingCostCriterion;
+
+    @ParameterGroup(alias = "Segmentation")
+    @Parameter(label = "Region merging criterion",
+            defaultValue = LOCAL_MUTUAL_BEST_FITTING_REGION_MERGING_CRITERION,
+            description = "The method to check the region merging.",
+            valueSet = {BEST_FITTING_REGION_MERGING_CRITERION, LOCAL_MUTUAL_BEST_FITTING_REGION_MERGING_CRITERION})
+    private String regionMergingCriterion;
+
+    @ParameterGroup(alias = "Segmentation")
+    @Parameter(label = "Total iterations",
+            defaultValue = "10",
+            description = "The total number of iterations.")
+    private int totalIterationsForSecondSegmentation;
+
+    @ParameterGroup(alias = "Segmentation")
+    @Parameter(label = "Threshold", defaultValue = "5.0", description = "The threshold.")
+    private float threshold;
+
+    @ParameterGroup(alias = "Segmentation")
+    @Parameter(label = "Spectral weight", defaultValue = "0.5", description = "The spectral weight.")
+    private float spectralWeight;
+
+    @ParameterGroup(alias = "Segmentation")
+    @Parameter(label = "Shape weight", defaultValue = "0.5" , description = "The shape weight.")
+    private float shapeWeight;
+
 
     @Override
     public void initialize() throws OperatorException {
