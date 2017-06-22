27b0d327607dc43cc74d4524d38598b0d991ed85
ramonamanda <ramona.monda@c-s.ro>
2017-06-22 09:05:44 +0300

    Updated ForestCoverChangeConstants class

--- s2tbx-forest-cover-change/src/main/java/org/esa/s2tbx/fcc/intern/ForestCoverChangeConstans.java
+++ s2tbx-forest-cover-change/src/main/java/org/esa/s2tbx/fcc/intern/ForestCoverChangeConstans.java
@@ -8,4 +8,8 @@ public class ForestCoverChangeConstans {
     public static final String LAND_COVER_NAME = "CCILandCover-2015";
     public static final int[] COVER_LABElS = new int[]{40, 50, 60, 61, 62, 70, 71, 72, 80, 81, 82, 90, 100, 110, 160, 170};
     public static final int NO_DATA_VALUE = 0;
+    public static final double CONFIDENCE_LEVEL_90 = 0.9d;
+    public static final double CONFIDENCE_LEVEL_95 = 0.95d;
+    public static final double CONFIDENCE_LEVEL_99 = 0.99d;
+    public static final int DEGREES_OF_FREEDOM = 4;
 }
