9f61354d0c51b431fceec1978af4aba9bce1711e
dagmarM <Dagmar.Mueller@brockmann-consult.de>
2018-04-17 11:53:50 +0200

    - relativePath is calculated once for the entire granule, not per tile. - the iteration scheme through the positions is changed to a simpler version, which is identical to the one for potential cloud shadows - identifyMountainShadow:    - the relativePath is oriented away from the sun. altitude0 should therefore be higher than altitude1 along this path. A shadow could occur at position index1.    - the comparison against relativeMinMountainHeights is the only necessary one to find all possible mountain shadows along the path.    - the entire approach is not working, because the resolution of the elevation model is not fine enough...

--- s2tbx-s2msi-idepix/src/main/java/org/esa/s2tbx/s2msi/idepix/operators/cloudshadow/MountainShadowFlagger.java
+++ s2tbx-s2msi-idepix/src/main/java/org/esa/s2tbx/s2msi/idepix/operators/cloudshadow/MountainShadowFlagger.java
@@ -14,7 +14,7 @@ class MountainShadowFlagger {
 
     static void flagMountainShadowArea(int productWidth, int productHeight, Rectangle sourceRectangle,
                                        Rectangle targetRectangle, float sourceSunZenith, float sourceSunAzimuth,
-                                       float[] sourceAltitude, int[] flagArray, float minAltitude, float maxAltitude) {
+                                       float[] sourceAltitude, int[] flagArray, float minAltitude, float maxAltitude, Point2D[] relativePath) {
         final int sourceWidth = sourceRectangle.width;
         final int sourceHeight = sourceRectangle.height;
 
@@ -26,11 +26,12 @@ class MountainShadowFlagger {
         }
         double sunZenithRad = Math.min(89.0, sunZenithIntermediate) * MathUtils.DTOR;
         double sunAzimuthRad = sourceSunAzimuth * MathUtils.DTOR;
-        final Point2D[] relativePath = CloudShadowUtils.getRelativePath(minAltitude, sunZenithRad, sunAzimuthRad,
+        /*final Point2D[] relativePath = CloudShadowUtils.getRelativePath(minAltitude, sunZenithRad, sunAzimuthRad,
                                                                         maxAltitude, sourceRectangle, targetRectangle,
                                                                         productHeight, productWidth,
                                                                         S2IdepixPreCloudShadowOp.spatialResolution, false,
                                                                         true);
+                                                                        */
         if (relativePath.length < 2) {
             return;
         }
@@ -40,7 +41,29 @@ class MountainShadowFlagger {
         if (maxAltitude - minAltitude < relativeMinMountainHeights[1]) {
             return;
         }
-        int xOffset = targetRectangle.x - sourceRectangle.x;
+
+        int xOffset = 0;
+        int yOffset = 0;
+
+        if (sourceSunAzimuth < 90) {
+            //start at upper right(not necessary, direction of search is appointed in identifyPotentialCloudShadow)
+            xOffset = targetRectangle.x - sourceRectangle.x;
+        } else if (sourceSunAzimuth < 180) {
+            //start at lower right (not necessary, direction of search is appointed in identifyPotentialCloudShadow)
+            xOffset = targetRectangle.x - sourceRectangle.x;
+            yOffset = targetRectangle.y - sourceRectangle.y;
+        } else if (sourceSunAzimuth < 270) {
+            //start at lower left (not necessary, direction of search is appointed in identifyPotentialCloudShadow)
+            yOffset = targetRectangle.y - sourceRectangle.y;
+        }
+        for(int i = xOffset; i < sourceWidth ; i++){
+            for ( int j = yOffset; j< sourceHeight; j++){
+                identifyMountainShadow(i, j, relativePath, relativeMinMountainHeights, sourceHeight, sourceWidth,
+                        sourceAltitude, flagArray, maxAltitude);
+            }
+        }
+
+        /*int xOffset = targetRectangle.x - sourceRectangle.x;
         int yOffset = targetRectangle.y - sourceRectangle.y;
         int width = targetRectangle.width;
         int height = targetRectangle.height;
@@ -96,17 +119,19 @@ class MountainShadowFlagger {
                 }
                 i++;
             }
-        }
+        }*/
     }
 
     static double[] getRelativeMinMountainHeights(Point2D[] relativePath, double spatialResolution, double sunZenith) {
         double[] relativeMinMountainHeights = new double[relativePath.length];
-        final double sunFactor = Math.tan(Math.PI / 2. - sunZenith);
+        //final double sunFactor = Math.tan(Math.PI / 2. - sunZenith);
+        final double sunFactor = Math.tan(sunZenith);
         for (int i = 1; i < relativePath.length; i++) {
             final double x = relativePath[i].getX();
             final double y = relativePath[i].getY();
             double dist = Math.sqrt(Math.pow(spatialResolution * x, 2) + Math.pow(spatialResolution * y, 2));
-            relativeMinMountainHeights[i] = dist * sunFactor;
+            //relativeMinMountainHeights[i] = dist * sunFactor;
+            relativeMinMountainHeights[i] = dist / sunFactor;
         }
         return relativeMinMountainHeights;
     }
@@ -116,8 +141,11 @@ class MountainShadowFlagger {
                                                float[] sourceAltitude, int[] flagArray, float maxAltitude) {
         int index0 = y0 * width + x0;
         float altitude0 = sourceAltitude[index0];
-        if (((flagArray[index0] & PreparationMaskBand.MOUNTAIN_SHADOW_FLAG) == PreparationMaskBand.MOUNTAIN_SHADOW_FLAG)
+        /*if (((flagArray[index0] & PreparationMaskBand.MOUNTAIN_SHADOW_FLAG) == PreparationMaskBand.MOUNTAIN_SHADOW_FLAG)
                 || Float.isNaN(altitude0)) {
+            return;
+        }*/
+        if ( Float.isNaN(altitude0)) {
             return;
         }
         for (int i = 1; i < relativePath.length; i++) {
@@ -134,9 +162,15 @@ class MountainShadowFlagger {
             if (Float.isNaN(altitude1)) {
                 continue;
             }
-            if (altitude1 - altitude0 > relativeMinMountainHeights[i]) {
-                flagArray[index0] += PreparationMaskBand.MOUNTAIN_SHADOW_FLAG;
-                for (int j = 1; j < i; j++) {
+            //if (altitude1 - altitude0 > relativeMinMountainHeights[i]) {
+            if (altitude0 - altitude1 > relativeMinMountainHeights[i]) {
+                if (!((flagArray[index1] & PreparationMaskBand.MOUNTAIN_SHADOW_FLAG) ==
+                        PreparationMaskBand.MOUNTAIN_SHADOW_FLAG)) {
+                    flagArray[index1] += PreparationMaskBand.MOUNTAIN_SHADOW_FLAG;
+                }
+
+                //for (int j = 1; j < i; j++) {
+                /*for (int j = i; j < relativePath.length; j++) {
                     int x2 = x0 + (int) relativePath[j].getX();
                     int y2 = y0 + (int) relativePath[j].getY();
                     final Point2D.Double relativePos = new Point2D.Double(x1 - x2, y1 - y2);
@@ -147,7 +181,7 @@ class MountainShadowFlagger {
                             flagArray[index2] += PreparationMaskBand.MOUNTAIN_SHADOW_FLAG;
                         }
                     }
-                }
+                }*/
                 return;
             }
         }
