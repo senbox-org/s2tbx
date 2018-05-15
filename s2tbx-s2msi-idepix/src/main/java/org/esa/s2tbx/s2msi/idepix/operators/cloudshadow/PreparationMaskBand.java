56ae0d2f06894aaf9f3b9466b4875a0289498973
dagmarM <Dagmar.Mueller@brockmann-consult.de>
2018-05-15 13:51:18 +0200

    Adding Flag of combined cloud shadows: coinciding clustered shadow areas and shifted cloud mask

--- s2tbx-s2msi-idepix/src/main/java/org/esa/s2tbx/s2msi/idepix/operators/cloudshadow/PreparationMaskBand.java
+++ s2tbx-s2msi-idepix/src/main/java/org/esa/s2tbx/s2msi/idepix/operators/cloudshadow/PreparationMaskBand.java
@@ -18,6 +18,7 @@ class PreparationMaskBand {
     static final int CLOUD_BUFFER_FLAG = (int) (Math.pow(2, S2IdepixPreCloudShadowOp.F_CLOUD_BUFFER) + 0.1);
     static final int POTENTIAL_CLOUD_SHADOW_FLAG = (int) (Math.pow(2, S2IdepixPreCloudShadowOp.F_POTENTIAL_CLOUD_SHADOW) + 0.1);
     static final int SHIFTED_CLOUD_SHADOW_FLAG = (int) (Math.pow(2, S2IdepixPreCloudShadowOp.F_SHIFTED_CLOUD_SHADOW) + 0.1);
+    static final int CLOUD_SHADOW_COMB_FLAG = (int) (Math.pow(2, S2IdepixPreCloudShadowOp.F_CLOUD_SHADOW_COMB) + 0.1);
 
     static void prepareMaskBand(int productWidth, int productHeight, Rectangle tileSourceRectangle, int[] flagArray,
                                 FlagDetector flagDetector) {
