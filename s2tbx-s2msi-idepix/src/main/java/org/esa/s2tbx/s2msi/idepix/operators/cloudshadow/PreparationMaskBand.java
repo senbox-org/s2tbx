2c20f416c2607bc298dd088cfcab8fdec2c78833
dagmarM <Dagmar.Mueller@brockmann-consult.de>
2018-06-05 10:21:20 +0200

    Adding the shifted cloud mask as a flag in cloud gaps.

--- s2tbx-s2msi-idepix/src/main/java/org/esa/s2tbx/s2msi/idepix/operators/cloudshadow/PreparationMaskBand.java
+++ s2tbx-s2msi-idepix/src/main/java/org/esa/s2tbx/s2msi/idepix/operators/cloudshadow/PreparationMaskBand.java
@@ -19,6 +19,7 @@ class PreparationMaskBand {
     static final int POTENTIAL_CLOUD_SHADOW_FLAG = (int) (Math.pow(2, S2IdepixPreCloudShadowOp.F_POTENTIAL_CLOUD_SHADOW) + 0.1);
     static final int SHIFTED_CLOUD_SHADOW_FLAG = (int) (Math.pow(2, S2IdepixPreCloudShadowOp.F_SHIFTED_CLOUD_SHADOW) + 0.1);
     static final int CLOUD_SHADOW_COMB_FLAG = (int) (Math.pow(2, S2IdepixPreCloudShadowOp.F_CLOUD_SHADOW_COMB) + 0.1);
+    static final int SHIFTED_CLOUD_SHADOW_GAPS_FLAG = (int) (Math.pow(2, S2IdepixPreCloudShadowOp.F_SHIFTED_CLOUD_SHADOW_GAPS) + 0.1);
 
     static void prepareMaskBand(int productWidth, int productHeight, Rectangle tileSourceRectangle, int[] flagArray,
                                 FlagDetector flagDetector) {
