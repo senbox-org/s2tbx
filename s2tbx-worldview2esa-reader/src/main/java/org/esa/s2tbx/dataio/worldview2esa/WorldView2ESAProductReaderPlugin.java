b2a75ce559600fcd197ea81a5526e689ca7a86d8
dstefanescu <denisa.stefanescu@c-s.ro>
2019-03-08 16:08:28 +0200

    WorldView2 ESA archive module renamed; add a way to calculate the band width and height (when not all tiles are delivered); refactoring.

---
+++ s2tbx-worldview2esa-reader/src/main/java/org/esa/s2tbx/dataio/worldview2esa/WorldView2ESAProductReaderPlugin.java
@@ -0,0 +1,62 @@
+package org.esa.s2tbx.dataio.worldview2esa;
+
+import org.esa.s2tbx.dataio.readers.BaseProductReaderPlugIn;
+import org.esa.s2tbx.dataio.worldview2esa.common.WorldView2ESAConstants;
+import org.esa.snap.core.dataio.ProductReader;
+import org.esa.snap.core.datamodel.RGBImageProfile;
+import org.esa.snap.core.datamodel.RGBImageProfileManager;
+
+import java.util.Locale;
+
+/**
+ * Plugin for reading WorldView ESA archive files
+ */
+
+public class WorldView2ESAProductReaderPlugin extends BaseProductReaderPlugIn {
+    private static final String COLOR_PALETTE_FILE_NAME = "WorldView2ESA_color_palette.cpd";
+
+    public WorldView2ESAProductReaderPlugin() {
+        super("org/esa/s2tbx/dataio/worldview2esa/" + WorldView2ESAProductReaderPlugin.COLOR_PALETTE_FILE_NAME);
+        this.folderDepth = 1;
+    }
+
+    @Override
+    public Class[] getInputTypes() {
+        return WorldView2ESAConstants.READER_INPUT_TYPES;
+    }
+
+    @Override
+    public ProductReader createReaderInstance() {
+        return new WorldView2ESAProductReader(this);
+    }
+
+    @Override
+    public String[] getFormatNames() {
+        return WorldView2ESAConstants.FORMAT_NAMES;
+    }
+
+    @Override
+    public String[] getDefaultFileExtensions() {
+        return WorldView2ESAConstants.DEFAULT_EXTENSIONS;
+    }
+
+    @Override
+    public String getDescription(Locale locale) {
+        return WorldView2ESAConstants.DESCRIPTION;
+    }
+
+    @Override
+    protected String[] getMinimalPatternList() {
+        return WorldView2ESAConstants.MINIMAL_PRODUCT_PATTERNS;
+    }
+
+    @Override
+    protected String[] getExclusionPatternList() {
+        return new String[0];
+    }
+
+    @Override
+    protected void registerRGBProfile() {
+        RGBImageProfileManager.getInstance().addProfile(new RGBImageProfile("WorldView", WorldView2ESAConstants.WORLDVIEW2_RGB_PROFILE));
+    }
+}
---
+++ s2tbx-worldview2esa-reader/src/test/java/org/esa/s2tbx/dataio/worldview2esa/WorldView2ESAProductReaderAcceptanceTest.java
@@ -0,0 +1,6 @@
+package org.esa.s2tbx.dataio.worldview2esa;
+
+import org.esa.snap.dataio.ProductReaderAcceptanceTest;
+
+public class WorldView2ESAProductReaderAcceptanceTest extends ProductReaderAcceptanceTest {
+}
