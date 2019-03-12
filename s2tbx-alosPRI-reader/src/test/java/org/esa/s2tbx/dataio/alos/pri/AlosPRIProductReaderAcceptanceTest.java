3bba54d19095af76e8d17eb1aeb6565721982cf8
Jean <jean.seyral@c-s.fr>
2019-03-12 18:55:46 +0100

    Remove Test class with dependency to snap-reader-tests

--- s2tbx-alosPRI-reader/src/test/java/org/esa/s2tbx/dataio/alos/pri/AlosPRIProductReaderAcceptanceTest.java
+++
@@ -1,6 +0,0 @@
-package org.esa.s2tbx.dataio.alos.pri;
-
-import org.esa.snap.dataio.ProductReaderAcceptanceTest;
-
-public class AlosPRIProductReaderAcceptanceTest extends ProductReaderAcceptanceTest {
-}
