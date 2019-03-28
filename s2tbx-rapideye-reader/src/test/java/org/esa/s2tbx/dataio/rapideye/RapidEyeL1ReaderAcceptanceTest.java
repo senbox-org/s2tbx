05f6ee61e67cad334df43000958ac8457d883f7d
dstefanescu <denisa.stefanescu@c-s.ro>
2019-03-28 17:31:06 +0200

    corection for readers (Alos AV2, Alos PRISM, Deimos, Ikonos, Kompsat, Pleiades, RapidEye, Spot) due to errors on ProductReaderAcceptanceTest (cherry picked from commit 7b87f01)

--- s2tbx-rapideye-reader/src/test/java/org/esa/s2tbx/dataio/rapideye/RapidEyeL1ReaderAcceptanceTest.java
+++ s2tbx-rapideye-reader/src/test/java/org/esa/s2tbx/dataio/rapideye/RapidEyeL1ReaderAcceptanceTest.java
@@ -17,8 +17,10 @@
 
 package org.esa.s2tbx.dataio.rapideye;
 
+import org.esa.snap.dataio.ProductReaderAcceptanceTest;
+
 /**
  * Created by kraftek on 9/11/2014.
  */
-public class RapidEyeL1ReaderAcceptanceTest {
+public class RapidEyeL1ReaderAcceptanceTest extends ProductReaderAcceptanceTest {
 }
--- s2tbx-spot-reader/src/test/java/org/esa/s2tbx/dataio/spot/SpotDimapReaderAcceptanceTest.java
+++ s2tbx-spot-reader/src/test/java/org/esa/s2tbx/dataio/spot/SpotDimapReaderAcceptanceTest.java
@@ -17,8 +17,10 @@
 
 package org.esa.s2tbx.dataio.spot;
 
+import org.esa.snap.dataio.ProductReaderAcceptanceTest;
+
 /**
  * Created by kraftek on 9/11/2014.
  */
-public class SpotDimapReaderAcceptanceTest {
+public class SpotDimapReaderAcceptanceTest extends ProductReaderAcceptanceTest{
 }
