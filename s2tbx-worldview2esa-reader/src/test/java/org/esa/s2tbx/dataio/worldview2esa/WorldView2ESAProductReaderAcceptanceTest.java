cbba401a5c62ce71e65a399dee9a4260de862911
dstefanescu <denisa.stefanescu@c-s.ro>
2019-04-17 09:49:30 +0300

    update WorldView2 reader tests

--- s2tbx-worldview2esa-reader/src/test/java/org/esa/s2tbx/dataio/worldview2esa/WorldView2ESAProductReaderAcceptanceTest.java
+++
@@ -1,6 +0,0 @@
-package org.esa.s2tbx.dataio.worldview2esa;
-
-import org.esa.snap.dataio.ProductReaderAcceptanceTest;
-
-public class WorldView2ESAProductReaderAcceptanceTest extends ProductReaderAcceptanceTest {
-}
