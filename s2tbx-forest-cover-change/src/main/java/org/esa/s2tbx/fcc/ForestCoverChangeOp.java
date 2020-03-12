ee4ec228bd6b5bfd90e1fa81fd3cbb185df29757
Oana Hogoiu <oana.hogoiu@c-s.ro>
2020-03-12 16:57:17 +0200

    SIITBX-315 & SIITBX-379 - update CoregistrationOp and ForestCoverChangeOp

--- s2tbx-forest-cover-change/src/main/java/org/esa/s2tbx/fcc/ForestCoverChangeOp.java
+++ s2tbx-forest-cover-change/src/main/java/org/esa/s2tbx/fcc/ForestCoverChangeOp.java
@@ -186,15 +186,10 @@ public class ForestCoverChangeOp extends Operator {
 
         this.threadCount = Runtime.getRuntime().availableProcessors() - 1;
         this.threadPool = Executors.newCachedThreadPool();
-
-        //20190325 workaround for using the operator in gpt, call directly in initialize the doExecute until GraphBuilder corrections
-        doExecute();
     }
 
-    //20190325 workaround for using the operator in gpt, do not override doExecute (until GraphBuilder corrections)
-    //@Override
-    //public void doExecute(ProgressMonitor pm) throws OperatorException {
-    public void doExecute() {
+    @Override
+    public void doExecute(ProgressMonitor pm) throws OperatorException {
         long startTime = System.currentTimeMillis();
 
         String folderPath = System.getProperty("fcc.temp.folder.path");
