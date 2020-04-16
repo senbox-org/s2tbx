938fc92efbfd53b696b9f43f54430f3113ec2b4b
sabine <sabine.embacher@brockmann-consult.de>
2020-04-16 18:09:19 +0200

    Copy StartTime and EndTime from source to target product

--- s2tbx-reflectance-to-radiance/src/main/java/org/esa/s2tbx/reflectance2radiance/ReflectanceToRadianceOp.java
+++ s2tbx-reflectance-to-radiance/src/main/java/org/esa/s2tbx/reflectance2radiance/ReflectanceToRadianceOp.java
@@ -2,7 +2,14 @@ package org.esa.s2tbx.reflectance2radiance;
 
 import com.bc.ceres.core.ProgressMonitor;
 import org.esa.s2tbx.dataio.s2.S2BandConstants;
-import org.esa.snap.core.datamodel.*;
+import org.esa.snap.core.datamodel.Band;
+import org.esa.snap.core.datamodel.Mask;
+import org.esa.snap.core.datamodel.MetadataAttribute;
+import org.esa.snap.core.datamodel.MetadataElement;
+import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.datamodel.ProductData;
+import org.esa.snap.core.datamodel.ProductNodeGroup;
+import org.esa.snap.core.datamodel.TiePointGrid;
 import org.esa.snap.core.gpf.Operator;
 import org.esa.snap.core.gpf.OperatorException;
 import org.esa.snap.core.gpf.OperatorSpi;
@@ -14,7 +21,8 @@ import org.esa.snap.core.gpf.annotations.TargetProduct;
 import org.esa.snap.core.util.ProductUtils;
 import org.esa.snap.utils.StringHelper;
 
-import java.awt.*;
+import java.awt.Rectangle;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -198,6 +206,7 @@ public class ReflectanceToRadianceOp extends Operator {
         /*targetProduct.setNumResolutionsMax(this.sourceProduct.getNumResolutionsMax());*/
         targetProduct.setNumResolutionsMax(distictWidths.size());
 
+        copyStartAndEndTime();
         ProductUtils.copyTiePointGrids(sourceProduct, targetProduct);
         ProductUtils.copyGeoCoding(sourceProduct, targetProduct);
         ProductUtils.copyFlagBands(sourceProduct, targetProduct, true);
@@ -283,6 +292,19 @@ public class ReflectanceToRadianceOp extends Operator {
 
     private boolean isSpotProduct(Product product) {
         return StringHelper.startsWithIgnoreCase(product.getProductType(), "SPOTSCENE");
+    }
+
+    private void copyStartAndEndTime() {
+        targetProduct.setStartTime(cloneUtc(sourceProduct.getStartTime()));
+        targetProduct.setEndTime(cloneUtc(sourceProduct.getEndTime()));
+    }
+
+    private ProductData.UTC cloneUtc(ProductData.UTC time) {
+        if (time == null) {
+            return null;
+        }
+        int[] ints = time.getArray();
+        return new ProductData.UTC(Arrays.copyOf(ints, ints.length));
     }
 
     private void copyMasks(Product sourceProduct, Product targetProduct, String... bandNames) {
