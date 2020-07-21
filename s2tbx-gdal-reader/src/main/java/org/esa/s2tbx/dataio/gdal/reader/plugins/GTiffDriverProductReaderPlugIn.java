65e26430a4d0e73ce58c7c571c33d23a5cc9dd7e
Cosmin Cara <kraftek@c-s.ro>
2020-07-21 17:52:58 +0300

    Enable GDAL GTiff reader for COGs. Prevent GeoTIFF/BigTIFF readers to read accidentally read COGs. Optimize reader plugin looping.

--- s2tbx-gdal-reader/src/main/java/org/esa/s2tbx/dataio/gdal/reader/plugins/GTiffDriverProductReaderPlugIn.java
+++ s2tbx-gdal-reader/src/main/java/org/esa/s2tbx/dataio/gdal/reader/plugins/GTiffDriverProductReaderPlugIn.java
@@ -1,6 +1,10 @@
 package org.esa.s2tbx.dataio.gdal.reader.plugins;
 
 import org.esa.s2tbx.dataio.gdal.GDALVersion;
+import org.esa.snap.core.dataio.DecodeQualification;
+import org.esa.snap.dataio.geotiff.Utils;
+
+import java.nio.file.Path;
 
 /**
  * Reader plugin for products using the GDAL library.
@@ -15,7 +19,22 @@ public class GTiffDriverProductReaderPlugIn extends AbstractDriverProductReaderP
         // in case this driver is enabled from GDAL
         super("GTiff", "GeoTIFF (GDAL " + GDALVersion.getGDALVersion().getId() + ")");
 
-        addExtensin(".tif");
-        addExtensin(".tiff");
+        addExtension(".tif");
+        addExtension(".tiff");
+    }
+
+    @Override
+    public DecodeQualification getDecodeQualification(Object input) {
+        final DecodeQualification qualification = super.getDecodeQualification(input);
+        if (qualification == DecodeQualification.UNABLE) {
+            return qualification;
+        }
+        final Path filePath = getInput(input);
+        try {
+            // 2020-07-21 CC Added COG check
+            return Utils.isCOGGeoTIFF(filePath) ? DecodeQualification.INTENDED : DecodeQualification.UNABLE;
+        } catch (Exception e) {
+            return DecodeQualification.UNABLE;
+        }
     }
 }
