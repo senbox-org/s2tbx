6d1c73251833309176db785f7990c02fa4a323bc
Marco Peters <marco.peters@brockmann-consult.de>
2021-05-18 11:49:40 +0200

    delegating to new implementation

--- s2tbx-commons/src/main/java/org/esa/snap/utils/NativeLibraryUtils.java
+++ s2tbx-commons/src/main/java/org/esa/snap/utils/NativeLibraryUtils.java
@@ -1,22 +1,8 @@
 package org.esa.snap.utils;
 
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-import org.esa.snap.core.util.StringUtils;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.URLDecoder;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 
 /**
@@ -28,40 +14,13 @@ import java.util.stream.Collectors;
  */
 @Deprecated()
 public class NativeLibraryUtils {
-    private static final String ENV_LIB_PATH = "java.library.path";
 
     public static void registerNativePaths(Path... paths) {
-        if (paths == null || paths.length == 0)
-            return;
-        registerNativePaths(Arrays.stream(paths).map(Path::toString).collect(Collectors.toList()).toArray(new String[paths.length]));
+        org.esa.snap.core.util.NativeLibraryUtils.registerNativePaths(Arrays.stream(paths).map(Path::toString).collect(Collectors.toList()).toArray(new String[paths.length]));
     }
 
     public static void registerNativePaths(String... paths) {
-        if (paths == null || paths.length == 0)
-            return;
-        String propertyValue = System.getProperty(ENV_LIB_PATH);
-        StringBuilder builder = new StringBuilder();
-        for (String path : paths) {
-            if (!StringUtils.isNullOrEmpty(propertyValue) &&
-                    (!propertyValue.contains(path) ||
-                            propertyValue.contains(path + File.separator))) {
-                builder.append(path).append(File.pathSeparator);
-            }
-        }
-        if (!StringUtils.isNullOrEmpty(propertyValue)) {
-            propertyValue = builder.toString() + propertyValue;
-        } else {
-            propertyValue = builder.toString();
-        }
-        System.setProperty(ENV_LIB_PATH, propertyValue);
-        try {
-            java.lang.reflect.Method initializePathMethod = PrivilegedAccessor.getMethod(ClassLoader.class, "initializePath", new Class[]{String.class});
-            initializePathMethod.setAccessible(true);
-            String[] updatedUsrPaths = (String[]) initializePathMethod.invoke(null, ENV_LIB_PATH);
-            PrivilegedAccessor.setStaticValue(ClassLoader.class, "usr_paths", updatedUsrPaths);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        org.esa.snap.core.util.NativeLibraryUtils.registerNativePaths(paths);
     }
 
     /**
@@ -73,61 +32,10 @@ public class NativeLibraryUtils {
      * @throws IOException              If temporary file creation or read/write operation fails
      */
     public static void loadLibrary(String path, String libraryName) throws IOException {
-        path = URLDecoder.decode(path, "UTF-8");
-        String mappedLibName = System.mapLibraryName(libraryName);
-        if (path.startsWith("/")) {
-            path = path.substring(1);
-        }
-        path = path.replace('/', File.separatorChar);
-        if (path.contains(".jar")) {
-            int contentsSeparatorIndex = path.indexOf("!");
-            String jarPath = path.substring(0, contentsSeparatorIndex);
-            JarFile jarFile = new JarFile(jarPath);
-            Enumeration<JarEntry> entries = jarFile.entries();
-            JarEntry jarEntry = null;
-            while (entries.hasMoreElements()) {
-                JarEntry currentEntry = entries.nextElement();
-                if (StringHelper.containsIgnoreCase(currentEntry.getName(), libraryName)) {
-                    jarEntry = currentEntry;
-                    break;
-                }
-            }
-            if (jarEntry == null) {
-                throw new IOException(String.format("Library %s could not be found in the jar file %s", libraryName, path));
-            }
-            try (InputStream in = jarFile.getInputStream(jarEntry)) {
-                Path tmpPath = Paths.get(System.getProperty("java.io.tmpdir"), "lib", getOSFamily(), mappedLibName);
-                try (OutputStream out = FileUtils.openOutputStream(tmpPath.toFile())) {
-                    IOUtils.copy(in, out);
-                }
-                path = tmpPath.toAbsolutePath().toString();
-            }
-        } else {
-            if (!Files.exists(Paths.get(path, mappedLibName))) {
-                throw new IOException(String.format("Library %s could not be found in %s", mappedLibName, path));
-            }
-        }
-        registerNativePaths(path);
-        System.loadLibrary(libraryName);
+        org.esa.snap.core.util.NativeLibraryUtils.loadLibrary(path, libraryName);
     }
 
     public static String getOSFamily() {
-        String ret;
-        String sysName = System.getProperty("os.name").toLowerCase();
-        String sysArch = System.getProperty("os.arch").toLowerCase();
-        if (sysName.contains("windows")) {
-            if (sysArch.contains("amd64") || sysArch.contains("x86_x64")) {
-                ret = "win64";
-            } else {
-                ret = "win32";
-            }
-        } else if (sysName.contains("linux")) {
-            ret = "linux";
-        } else if (sysName.contains("mac")) {
-            ret = "macosx";
-        } else {
-            throw new NotImplementedException();
-        }
-        return ret;
+        return org.esa.snap.core.util.NativeLibraryUtils.getOSFamily();
     }
 }
