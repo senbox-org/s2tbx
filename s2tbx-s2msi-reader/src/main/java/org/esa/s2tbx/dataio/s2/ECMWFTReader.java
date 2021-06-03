e2d6c005f4b9607b70c4f38b969be3ac09d81e28
Cosmin Cara <kraftek@c-s.ro>
2021-06-03 15:39:35 +0300

    Shorten cache paths wherever possible; fixed an issue with reading S2 products from zip file (mixed file system providers caused NoSuchProvider exception)

--- s2tbx-s2msi-reader/src/main/java/org/esa/s2tbx/dataio/s2/ECMWFTReader.java
+++ s2tbx-s2msi-reader/src/main/java/org/esa/s2tbx/dataio/s2/ECMWFTReader.java
@@ -2,13 +2,10 @@ package org.esa.s2tbx.dataio.s2;
 
 import org.apache.commons.io.FileUtils;
 import org.esa.snap.core.datamodel.TiePointGrid;
-import org.opengis.referencing.FactoryException;
-import org.opengis.referencing.operation.TransformException;
 import ucar.nc2.NetcdfFile;
 import ucar.nc2.Variable;
 
 import java.io.IOException;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
@@ -17,34 +14,36 @@ import java.util.List;
 
 public class ECMWFTReader {
 
-    List<TiePointGrid> tiePointGrid;
+    final List<TiePointGrid> tiePointGrid;
 
     public ECMWFTReader(Path path, Path cachedir) throws IOException {
-        if (tiePointGrid != null)
-            tiePointGrid.clear();
-        tiePointGrid = new ArrayList<>();
+        this.tiePointGrid = new ArrayList<>();
         NetcdfFile ncfile = null;
-        Path cacheFolderPath = cachedir;
-        cacheFolderPath = cacheFolderPath.resolve("aux_ecmwft");
+        final Path cacheFolderPath = cachedir.resolve("aux_ecmfwt");
         try {
-            Files.createDirectory(cacheFolderPath);
-        } catch (FileAlreadyExistsException exc) {
-        }
-        Path copyPath = cacheFolderPath.resolve(path.getFileName().toString());
-        try {
+            final Path copyPath = cacheFolderPath.resolve(path.getFileName().toString());
+            Files.createDirectories(copyPath);
             Files.copy(path, copyPath, StandardCopyOption.REPLACE_EXISTING);
             ncfile = NetcdfFile.openInMemory(copyPath.toString());
-            tiePointGrid.add(getGrid(ncfile, "Total_column_water_vapour_surface"));
-            tiePointGrid.add(getGrid(ncfile, "Total_column_ozone_surface"));
-            tiePointGrid.add(getGrid(ncfile, "Mean_sea_level_pressure_surface"));
-        } catch (IOException ioe) {
+            TiePointGrid tpGrid = getGrid(ncfile, "Total_column_water_vapour_surface");
+            if (tpGrid != null) {
+                this.tiePointGrid.add(tpGrid);
+            }
+            tpGrid = getGrid(ncfile, "Total_column_ozone_surface");
+            if (tpGrid != null) {
+                this.tiePointGrid.add(tpGrid);
+            }
+            tpGrid = getGrid(ncfile, "Mean_sea_level_pressure_surface");
+            if (tpGrid != null) {
+                this.tiePointGrid.add(tpGrid);
+            }
+        } catch (Exception ioe) {
             // Handle less-cool exceptions here
             ioe.printStackTrace();
-        } catch (Exception e) {
-            // Handle less-cool exceptions here
-            e.printStackTrace();
         } finally {
-            ncfile.close();
+            if (ncfile != null) {
+                ncfile.close();
+            }
             FileUtils.deleteDirectory(cacheFolderPath.toFile());
         }
     }
@@ -53,16 +52,18 @@ public class ECMWFTReader {
         return tiePointGrid;
     }
 
-    public TiePointGrid getGrid(NetcdfFile ncfile, String name) throws IOException, FactoryException, TransformException {
-       
-        Variable variable = ncfile.findVariable(null, name);
+    public TiePointGrid getGrid(NetcdfFile ncfile, String name) throws IOException {
+        final Variable variable = ncfile.findVariable(null, name);
+        if (variable == null) {
+            return null;
+        }
         String description = variable.getDescription();
         if( description.contains("@"))
-            description = description.split(" @")[0]+" at surface level provided by ECMWF";
+            description = description.split(" @")[0] + " at surface level provided by ECMWF";
         String units = variable.getUnitsString();
-        int[] shape = variable.getShape();
-        float[] tiePoints = (float[])variable.read().get1DJavaArray(float.class);
-        TiePointGrid tiePointGrid = new TiePointGrid(name.toLowerCase().replace("_surface",""), shape[1],shape[2], 0.5, 0.5, 1220, 1220,tiePoints);
+        final int[] shape = variable.getShape();
+        final float[] tiePoints = (float[])variable.read().get1DJavaArray(float.class);
+        final TiePointGrid tiePointGrid = new TiePointGrid(name.toLowerCase().replace("_surface",""), shape[1],shape[2], 0.5, 0.5, 1220, 1220,tiePoints);
         tiePointGrid.setUnit(units);
         tiePointGrid.setNoDataValue(Double.NaN);
         tiePointGrid.setNoDataValueUsed(true);
