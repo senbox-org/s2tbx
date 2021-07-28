03625c0080368d8f96a909b01a7b6f6c72b153a6
Adrian DRAGHICI <adraghici@c-s.ro>
2021-07-29 00:06:00 +0300

    SIITBX-457: Fix issue with loading GDAL native libraries when multiple GDAL versions are installed by allowing user to chose one.

--- lib-gdal/src/main/java/org/esa/s2tbx/dataio/gdal/GDALLoaderConfig.java
+++ lib-gdal/src/main/java/org/esa/s2tbx/dataio/gdal/GDALLoaderConfig.java
@@ -14,14 +14,18 @@ public class GDALLoaderConfig {
 
     private static final String INSTANCE_CONFIG = "s2tbx";
     private static final String PREFERENCE_KEY_USE_INSTALLED_GDAL = "s2tbx.dataio.gdal.installed";
+    private static final String PREFERENCE_KEY_SELECTED_INSTALLED_GDAL = "s2tbx.dataio.gdal.installed.version";
 
     private static final Logger logger = Logger.getLogger(GDALLoaderConfig.class.getName());
     private static final GDALLoaderConfig instance = new GDALLoaderConfig();
+    public static final String PREFERENCE_NONE_VALUE_SELECTED_INSTALLED_GDAL = "none";
 
     private boolean useInstalledGDALLibrary;
+    private String selectedInstalledGDALLibrary;
 
     private GDALLoaderConfig() {
-        this.useInstalledGDALLibrary = getUseInstalledGDALLibrary();
+        this.useInstalledGDALLibrary = loadUseInstalledGDALLibrary();
+        this.selectedInstalledGDALLibrary = loadSelectedInstalledGDALLibrary();
     }
 
     /**
@@ -38,9 +42,19 @@ public class GDALLoaderConfig {
      *
      * @return the setting for using installed distribution
      */
-    private boolean getUseInstalledGDALLibrary() {
+    private boolean loadUseInstalledGDALLibrary() {
         final Preferences preferences = Config.instance(INSTANCE_CONFIG).load().preferences();
         return preferences.getBoolean(PREFERENCE_KEY_USE_INSTALLED_GDAL, true);
+    }
+
+    /**
+     * Fetches the setting for selecting installed distribution, from SNAP config files.
+     *
+     * @return the setting for selecting installed distribution
+     */
+    private String loadSelectedInstalledGDALLibrary() {
+        final Preferences preferences = Config.instance(INSTANCE_CONFIG).load().preferences();
+        return preferences.get(PREFERENCE_KEY_SELECTED_INSTALLED_GDAL, PREFERENCE_NONE_VALUE_SELECTED_INSTALLED_GDAL);
     }
 
     /**
@@ -60,6 +74,22 @@ public class GDALLoaderConfig {
     }
 
     /**
+     * Sets the setting for selecting installed distribution, to SNAP config files.
+     *
+     * @param selectedInstalledGDALLibrary the setting for selecting installed distribution
+     */
+    public void setSelectedInstalledGDALLibrary(String selectedInstalledGDALLibrary) {
+        this.selectedInstalledGDALLibrary = selectedInstalledGDALLibrary;
+        final Preferences preferences = Config.instance(INSTANCE_CONFIG).load().preferences();
+        preferences.put(PREFERENCE_KEY_SELECTED_INSTALLED_GDAL, this.selectedInstalledGDALLibrary);
+        try {
+            preferences.flush();
+        } catch (Exception e) {
+            logger.severe(e.getMessage());
+        }
+    }
+
+    /**
      * Gets whether installed distribution is set to be used by SNAP.
      *
      * @return {@code true} if installed distribution is set to be used by SNAP
@@ -68,4 +98,12 @@ public class GDALLoaderConfig {
         return this.useInstalledGDALLibrary;
     }
 
+    /**
+     * Gets the selected installed distribution to be used by SNAP.
+     *
+     * @return the selected installed distribution to be used by SNAP
+     */
+    public String getSelectedInstalledGDALLibrary() {
+        return selectedInstalledGDALLibrary;
+    }
 }
