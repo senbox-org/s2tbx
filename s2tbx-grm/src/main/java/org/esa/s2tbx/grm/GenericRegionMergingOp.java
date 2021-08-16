6cafd26cf05f999883115829af886d881c98e150
Adrian DRAGHICI <adraghici@c-s.ro>
2021-08-16 17:37:28 +0300

    SIITBX-434: Fix GenericRegionMerging GPT tests failed.

--- s2tbx-grm/src/main/java/org/esa/s2tbx/grm/GenericRegionMergingOp.java
+++ s2tbx-grm/src/main/java/org/esa/s2tbx/grm/GenericRegionMergingOp.java
@@ -362,7 +362,7 @@ public class GenericRegionMergingOp extends Operator {
         int tileCountY = MathUtils.ceilInt(sceneHeight / (double) tileSize.height);
         this.totalTileCount = tileCountX * tileCountY;
 
-        int threadCount = Runtime.getRuntime().availableProcessors() - 1;
+        int threadCount = 1;
         Executor threadPool = Executors.newCachedThreadPool();
 
         String folderPath = System.getProperty("grm.temp.folder.path");
