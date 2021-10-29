e693775ad82e98a57b2fee6fa3740da46d877a8b
Florian1209 <douziechflorian@gmail.com>
2021-10-29 10:49:18 +0200

    s2Resampler: get deftoo resolution for new S2A Band1 resolution

--- s2tbx-s2msi-reader/src/main/java/org/esa/s2tbx/dataio/s2/ortho/Sentinel2OrthoProductReader.java
+++ s2tbx-s2msi-reader/src/main/java/org/esa/s2tbx/dataio/s2/ortho/Sentinel2OrthoProductReader.java
@@ -513,7 +513,7 @@ public abstract class Sentinel2OrthoProductReader extends Sentinel2ProductReader
         int bandIndexNumber = 0;
         double mosaicOpSourceThreshold = 1.0d;
         String[] offsets = null;
-        if((productCharacteristics.getPsd()>147) && S2OrthoUtils.reverseNegativeOffset()) {
+        if((productCharacteristics.getPsd()>147) && S2OrthoUtils.addNegativeOffset()) {
             offsets =productCharacteristics.getOffsetList();
             if(offsets==null)
                 logger.warning("The metadata offset values are not accessible.");
